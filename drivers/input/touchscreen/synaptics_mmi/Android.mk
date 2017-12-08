89c664fc483238348b4b2ba25ce694535b73c18d
Konstantin Makariev <kmakariev@motorola.com>
2017-12-08 14:18:27 -0600

    motorola: dlkm: synaptics touch driver
    Moving Synaptics touchscreen driver from kernel to DLKM
    
    Change-Id: Ia72f58cd400fb2c51b6170a8e0b43ceb16c72c94
    Signed-off-by: Konstantin Makariev <kmakariev@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1102121
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Submit-Approved: Jira Key

---
+++ drivers/input/touchscreen/synaptics_mmi/Android.mk
@@ -0,0 +1,27 @@
+DLKM_DIR := motorola/kernel/modules
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := synaptics_mmi.ko
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
+include $(DLKM_DIR)/AndroidKernelModule.mk
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := synaptics_dsx_fw_update.ko
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
+include $(DLKM_DIR)/AndroidKernelModule.mk
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := synaptics_dsx_rmi_dev.ko
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
+include $(DLKM_DIR)/AndroidKernelModule.mk
+
+#This requires F54 support, thus disable it for now
+#include $(CLEAR_VARS)
+#LOCAL_MODULE := synaptics_dsx_test_reporting.ko
+#LOCAL_MODULE_TAGS := optional
+#LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
+#include $(DLKM_DIR)/AndroidKernelModule.mk
