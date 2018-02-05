abbf0fe09f4959cdda7cafa7e958d5b200f700b9
Vlad Karpovich <Vlad.Karpovich@cirrus.com>
2018-02-05 15:14:35 -0600

    ASoC: madera: Add Cirrus Logic audio drivers
    The following drivers are included and modyfied to be run
    as DLKM:
    Core madera drivers,
    extcon driver,
    CS47L35 and CS47L90 codec drivers,
    CS35l36 AMP driver,
    CS40L20 haptic driver.
    
    Change-Id: I7a285d434244e08635a2cfdcd3f21f45c3d8c0f6
    Signed-off-by: Vlad Karpovich <Vlad.Karpovich@cirrus.com>
    Signed-off-by: Ravi Vembu <raviv@motorola.com>
    Mot-CRs-fixed: (CR)
    Reviewed-on: https://gerrit.mot.com/1131231
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Kenneth Kessler <kennykessler@motorola.com>
    Submit-Approved: Jira Key

---
+++ sound/soc/codecs/Android.mk
@@ -0,0 +1,35 @@
+DLKM_DIR := motorola/kernel/modules
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := cirrus_madera.ko
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
+include $(DLKM_DIR)/AndroidKernelModule.mk
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := cirrus_cs35l36.ko
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
+include $(DLKM_DIR)/AndroidKernelModule.mk
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := cirrus_cs47l35.ko
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
+include $(DLKM_DIR)/AndroidKernelModule.mk
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := cirrus_cs47l90.ko
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
+include $(DLKM_DIR)/AndroidKernelModule.mk
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := aov_trigger.ko
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
+include $(DLKM_DIR)/AndroidKernelModule.mk
+
+
+
