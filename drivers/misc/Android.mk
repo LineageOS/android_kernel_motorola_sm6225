6206bf1cb727c6cddc397b0efc8b3a218c7fd7e1
Kenny Kessler <kenny.kessler@motorola.com>
2018-02-08 16:33:12 -0600

    modules: misc: Include Subfolders
    Need the Android.mk files from subfolders as well.
    
    Change-Id: I97fb12df4732da9798e8e403cbaa72d60fedfd1d
    Signed-off-by: Kenny Kessler <kenny.kessler@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1132962
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Ryan Lattrel <ryanl@motorola.com>
    Reviewed-by: Kenneth Kessler <kennykessler@motorola.com>
    Submit-Approved: Jira Key

--- drivers/misc/Android.mk
+++ drivers/misc/Android.mk
@@ -12,3 +12,5 @@ include $(DLKM_DIR)/AndroidKernelModule.mk
 # LOCAL_MODULE_TAGS := optional
 # LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
 # include $(DLKM_DIR)/AndroidKernelModule.mk
+
+include $(call first-makefiles-under,$(LOCAL_PATH))
