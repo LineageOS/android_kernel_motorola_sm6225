538c36b514ac9735cd6c05a095eb80dbbc75339b
Vlad Karpovich <vlad.karpovich@cirrus.com>
2018-04-04 16:45:11 -0500

    ASoC: cs35l41: Cirrus cs35l41 amplifier driver
    11-V Boosted mono class D amplifuer with DSP speaker
    protection and equalization
    
    Change-Id: Ic8e2fe2a5a024e6b19fd97db9ece814a8764523c
    Reviewed-on: https://gerrit.mot.com/1166691
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Yuesheng Huang <huangys@motorola.com>
    Reviewed-by: Guobin Zhang <zhanggb@motorola.com>
    Submit-Approved: Jira Key

--- sound/soc/codecs/Android.mk
+++ sound/soc/codecs/Android.mk
@@ -20,6 +20,12 @@ LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
 include $(DLKM_DIR)/AndroidKernelModule.mk
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := cirrus_cs35l41.ko
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
+include $(DLKM_DIR)/AndroidKernelModule.mk
+
+include $(CLEAR_VARS)
 LOCAL_MODULE := cirrus_cs47l35.ko
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
