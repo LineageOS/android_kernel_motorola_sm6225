1d851e36c7d2f297518bd6465831a5ab53382e9d
Vlad Karpovich <Vlad.Karpovich@cirrus.com>
2018-05-18 15:06:00 -0500

    ASoC: cs35l41: Change from MFD to codec driver.
    Remove MFD driver support from cs35l41 codec.
    
    Change-Id: Iffcb54eac647a4aa7e47b37ffda8ded1e06af8cf
    Signed-off-by: Vlad Karpovich <Vlad.Karpovich@cirrus.com>
    Mot-CRs-fixed: (CR)
    Reviewed-on: https://gerrit.mot.com/1177445
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Ravikumar Vembu <raviv@motorola.com>
    Submit-Approved: Jira Key

--- drivers/mfd/Android.mk
+++ drivers/mfd/Android.mk
@@ -7,8 +7,3 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
 include $(DLKM_DIR)/AndroidKernelModule.mk
 
-include $(CLEAR_VARS)
-LOCAL_MODULE := cirrus_cs35l41-mfd.ko
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
-include $(DLKM_DIR)/AndroidKernelModule.mk
--- sound/soc/codecs/cs35l41-i2c.c
+++ sound/soc/codecs/cs35l41-i2c.c
@@ -1,26 +1,36 @@
 /*
- * I2C bus interface to Cirrus Logic CS35L41 codec
+ * cs35l41-i2c.c -- CS35l41 I2C driver
  *
- * Copyright 2017 Cirrus Logic
+ * Copyright 2017 Cirrus Logic, Inc.
  *
  * Author:	David Rhodes	<david.rhodes@cirrus.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
  * published by the Free Software Foundation.
+ *
  */
 
-#include <linux/device.h>
-#include <linux/err.h>
-#include <linux/i2c.h>
 #include <linux/module.h>
+#include <linux/moduleparam.h>
+#include <linux/version.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/delay.h>
+#include <linux/i2c.h>
+#include <linux/slab.h>
+#include <linux/workqueue.h>
+#include <linux/platform_device.h>
+#include <linux/regulator/consumer.h>
+#include <linux/gpio/consumer.h>
+#include <linux/of_device.h>
+#include <linux/of_gpio.h>
 #include <linux/regmap.h>
-#include <linux/of.h>
+#include <linux/gpio.h>
 
-#include <linux/mfd/cs35l41/registers.h>
-#include "sound/cs35l41.h"
+#include "wm_adsp.h"
 #include "cs35l41.h"
-
+#include <sound/cs35l41.h>
 
 static struct regmap_config cs35l41_regmap_i2c = {
 	.reg_bits = 32,
@@ -36,13 +46,6 @@ static struct regmap_config cs35l41_regmap_i2c = {
 	.cache_type = REGCACHE_RBTREE,
 };
 
-static const struct of_device_id cs35l41_of_match[] = {
-	{.compatible = "cirrus,cs35l40"},
-	{.compatible = "cirrus,cs35l41"},
-	{},
-};
-MODULE_DEVICE_TABLE(of, cs35l41_of_match);
-
 static const struct i2c_device_id cs35l41_id_i2c[] = {
 	{"cs35l40", 0},
 	{"cs35l41", 0},
@@ -54,20 +57,20 @@ MODULE_DEVICE_TABLE(i2c, cs35l41_id_i2c);
 static int cs35l41_i2c_probe(struct i2c_client *client,
 				const struct i2c_device_id *id)
 {
+	struct cs35l41_private *cs35l41;
 	struct device *dev = &client->dev;
 	struct cs35l41_platform_data *pdata = dev_get_platdata(dev);
 	const struct regmap_config *regmap_config = &cs35l41_regmap_i2c;
-	struct cs35l41_data *cs35l41;
 	int ret;
 
-	cs35l41 = devm_kzalloc(dev, sizeof(struct cs35l41_data), GFP_KERNEL);
+	cs35l41 = devm_kzalloc(dev, sizeof(struct cs35l41_private), GFP_KERNEL);
 
 	if (cs35l41 == NULL)
 		return -ENOMEM;
 
 	cs35l41->dev = dev;
 	cs35l41->irq = client->irq;
-	cs35l41->pdata = pdata;
+	cs35l41->bus_spi = false;
 
 	i2c_set_clientdata(client, cs35l41);
 	cs35l41->regmap = devm_regmap_init_i2c(client, regmap_config);
@@ -78,15 +81,26 @@ static int cs35l41_i2c_probe(struct i2c_client *client,
 		return ret;
 	}
 
-	return cs35l41_dev_init(cs35l41);
+	return cs35l41_probe(cs35l41, pdata);
 }
 
 static int cs35l41_i2c_remove(struct i2c_client *client)
 {
-	struct cs35l41_data *cs35l41 = i2c_get_clientdata(client);
+	struct cs35l41_private *cs35l41 = i2c_get_clientdata(client);
 
-	return cs35l41_dev_exit(cs35l41);
+	regmap_write(cs35l41->regmap, CS35L41_IRQ1_MASK1, 0xFFFFFFFF);
+	wm_adsp2_remove(&cs35l41->dsp);
+	regulator_bulk_disable(cs35l41->num_supplies, cs35l41->supplies);
+	snd_soc_unregister_codec(cs35l41->dev);
+	return 0;
 }
+
+static const struct of_device_id cs35l41_of_match[] = {
+	{.compatible = "cirrus,cs35l40"},
+	{.compatible = "cirrus,cs35l41"},
+	{},
+};
+MODULE_DEVICE_TABLE(of, cs35l41_of_match);
 
 static struct i2c_driver cs35l41_i2c_driver = {
 	.driver = {
