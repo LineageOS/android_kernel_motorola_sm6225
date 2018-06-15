27e46b7985ae73327439613136ab4baf7de0d49b
liaohj <liaohj@motorola.com>
2018-06-15 18:14:22 +0800

    Ocean NA: audio bringup ,add tfa9874 driver
    add tfa9874 driver as modules.
    
    Change-Id: I53bc97d1435f44033cb4a17a7258ce38905a5b2f
    Signed-off-by: liaohj <liaohj@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1194107
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Wanlong Zhou <zhouwl@motorola.com>
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Submit-Approved: Jira Key

--- sound/soc/codecs/Android.mk
+++ sound/soc/codecs/Android.mk
@@ -60,3 +60,8 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
 include $(DLKM_DIR)/AndroidKernelModule.mk
 
+include $(CLEAR_VARS)
+LOCAL_MODULE := snd_soc_tfa9874.ko
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
+include $(DLKM_DIR)/AndroidKernelModule.mk
