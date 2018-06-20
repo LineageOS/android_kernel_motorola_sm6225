25fb12224c790189109725321c76e11d24936536
haojl2 <haojl2@motorola.com>
2018-06-20 16:44:13 +0800

    input: ets_fps_mmi: Inital egis fps driver
    Poring Egis fps driver.
    
    Change-Id: I6313631950869d7d44dc155538034ba4a85f323f
    Signed-off-by: haojl2 <haojl2@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1181700
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Jianqi Yang <yangj@motorola.com>
    Submit-Approved: Jira Key

--- drivers/input/misc/ets_fps_mmi/Android.mk
+++ drivers/input/misc/ets_fps_mmi/Android.mk
@@ -2,9 +2,8 @@ DLKM_DIR := motorola/kernel/modules
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := ldc2114_keys.ko
+LOCAL_MODULE := ets_fps_mmi.ko
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
 include $(DLKM_DIR)/AndroidKernelModule.mk
 
-include $(call first-makefiles-under,$(LOCAL_PATH))
