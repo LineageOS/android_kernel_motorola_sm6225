919691290f256a66c47e698344e8173ceece403e
Ryan Lattrel <ryanl@motorola.com>
2018-06-28 16:08:47 -0500

    drivers: mmi_annotate: fix 4.14 build error
    Update includes to resolve kernel 4.14 build.
    
    Change-Id: I8236ab966120d42292916ccc2f5177f9e32aa8d6
    Signed-off-by: Ryan Lattrel <ryanl@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1201402
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Kenneth Kessler <kennykessler@motorola.com>
    Submit-Approved: Jira Key

--- drivers/mmi_annotate/mmi_annotate.c
+++ drivers/mmi_annotate/mmi_annotate.c
@@ -11,7 +11,7 @@
  * GNU General Public License for more details.
  *
  */
-#include <asm/uaccess.h>
+#include <linux/uaccess.h>
 #include <linux/proc_fs.h>
 #include <linux/errno.h>
 #include <linux/kernel.h>
