32c77b4810a66b138a7d4c8c203b57ac87a8599e
Wen Xie <xiewen3@motorola.com>
2018-07-04 13:48:43 +0800

    ASoC: Tas2560: Add codec algo.
    Add audio codecs Tas2560 algo driver.
    
    Change-Id: Ib2aa0b0594fd551345b5bccde192ba9e7be9c062
    Signed-off-by: Wen Xie <xiewen3@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1203896
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Reviewed-by: Zhengming Yao <yaozm1@lenovo.com>
    Submit-Approved: Jira Key

--- sound/soc/codecs/Android.mk
+++ sound/soc/codecs/Android.mk
@@ -71,3 +71,15 @@ LOCAL_MODULE := tas2560.ko
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
 include $(DLKM_DIR)/AndroidKernelModule.mk
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := tas2560_algo.ko
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
+include $(DLKM_DIR)/AndroidKernelModule.mk
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := tas2560_calib.ko
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
+include $(DLKM_DIR)/AndroidKernelModule.mk
