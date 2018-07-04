dc688a2e607f08de81f68f0c7d3f3fcc201f0254
yangjq <yangjq@lenovo.com>
2018-07-04 19:36:33 +0800

    Add a macro for copying aw869x.ko to vendor/lib/modules
    Add a macro to select the destination path for copying aw869x.ko
    
    Change-Id: I7efbb1933f158673c049a1a0f2aaf64f6421144e
    Signed-off-by: yangjq <yangjq@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/1204257
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Tao Sun <suntao2@motorola.com>
    Reviewed-by: Wentao Xu <xuwt2@motorola.com>
    Submit-Approved: Jira Key

--- drivers/misc/awinic/Android.mk
+++ drivers/misc/awinic/Android.mk
@@ -4,5 +4,11 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := aw869x.ko
 LOCAL_MODULE_TAGS := optional
+
+ifeq ($(DLKM_INSTALL_TO_VENDOR_OUT),true)
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR)/lib/modules/
+else
 LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
+endif
+
 include $(DLKM_DIR)/AndroidKernelModule.mk
