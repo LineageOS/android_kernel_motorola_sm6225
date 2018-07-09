6c306b773f4326adda31ae2c75375293893741e1
zoujc <zoujc@motorola.com>
2018-07-09 11:15:11 +0800

    Lake:Thermal: Support mmi_sys_temp
    Fixed error: "Thermal-IOCTL: System Temp IOCTL not initialized"
    
    Change-Id: Iaecc120714736e7eba97dbbacb56b6ff9b6c87b5
    Signed-off-by: zoujc <zoujc@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1205917
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Jianqi Yang <yangj@motorola.com>
    Submit-Approved: Jira Key
    (cherry picked from commit 8cee4d3d36d34a72de8ea720865eb6f8adf13915)
    Reviewed-on: https://gerrit.mot.com/1213239

--- drivers/misc/mmi_sys_temp/Android.mk
+++ drivers/misc/mmi_sys_temp/Android.mk
@@ -4,6 +4,10 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := mmi_sys_temp.ko
 LOCAL_MODULE_TAGS := optional
+ifeq ($(DLKM_INSTALL_TO_VENDOR_OUT),true)
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR)/lib/modules/
+else
 LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
+endif
 
 include $(DLKM_DIR)/AndroidKernelModule.mk
