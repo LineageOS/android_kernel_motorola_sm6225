182442643adad054a8bfe0c74d4094861faaf825
haojl2 <haojl2@motorola.com>
2018-07-12 11:00:30 +0800

    Revert "(CR): synaptics_tcm: Add init FW to River."
    This reverts commit 9191d8448c01cd7d683b4bca77e2aeb81bb5ce00.
    
    Change-Id: I6a564979f92b6ff8cb1bbc0689581c71572602cd
    Reviewed-on: https://gerrit.mot.com/1208503
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Jianqi Yang <yangj@motorola.com>
    Submit-Approved: Jira Key

--- drivers/input/touchscreen/synaptics_tcm_mmi/Android.mk
+++ drivers/input/touchscreen/synaptics_tcm_mmi/Android.mk
@@ -46,36 +46,9 @@ LOCAL_MODULE_PATH := $(SYNA_TCM_MODULE_PATH)
 include $(DLKM_DIR)/AndroidKernelModule.mk
 
 include $(CLEAR_VARS)
- FIRMWARE_FILES := $(shell cd $(LOCAL_PATH) ; find -L -maxdepth 1 -name "*.img")
-
-# Destination folder in O is different
-ifeq ($(call is-platform-version-at-least,O),true)
-FIRMWARE_DEST_PATH = $(TARGET_OUT_VENDOR)/firmware/synaptics
-else
-FIRMWARE_DEST_PATH = $(TARGET_OUT_ETC)/firmware/synaptics
-endif
-
-define install-firmware-file
-$(eval $(install-to-destination))
-endef
-
-define install-to-destination
-     include $$(CLEAR_VARS)
-     LOCAL_MODULE_TAGS := optional
-     LOCAL_MODULE := $(1)
-     LOCAL_MODULE_PATH := $$(FIRMWARE_DEST_PATH)
-     LOCAL_MODULE_CLASS := ETC
-     LOCAL_SRC_FILES := $$(LOCAL_MODULE)
-     include $$(BUILD_PREBUILT)
-endef
-
- $(foreach f,$(FIRMWARE_FILES),$(call install-firmware-file,$(f)))
-
-include $(CLEAR_VARS)
 LOCAL_MODULE := synaptics_tcm_reflash.ko
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_PATH := $(SYNA_TCM_MODULE_PATH)
-LOCAL_REQUIRED_MODULES := $(FIRMWARE_FILES)
 include $(DLKM_DIR)/AndroidKernelModule.mk
 
 include $(CLEAR_VARS)
