8b33dd33e45d3b81656a19158a870174b7a22fe4
Jiyue Cui <cuijy2@lenovo.com>
2018-09-28 23:35:21 -0500

    Revert "add DLKM capsense sx933x for lake"
    something worng in sx933x_sar DLKM ko
    need revert this befor fixed the error
    
    This reverts commit fe4ce396d7c7029467e08e83c90fe58a61ff5314.
    
    Change-Id: Iad3ff050a58ec04f7b61b2efe6e6d1851c3cda1c
    Reviewed-on: https://gerrit.mot.com/1248677
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Guoyue Liu <liugy11@motorola.com>
    Reviewed-by: Tao Sun <suntao2@motorola.com>
    Submit-Approved: Jira Key

--- drivers/sensors/Android.mk
+++ drivers/sensors/Android.mk
@@ -4,11 +4,5 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := sensors_class.ko
 LOCAL_MODULE_TAGS := optional
-
-ifeq ($(DLKM_INSTALL_TO_VENDOR_OUT),true)
-LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR)/lib/modules/
-else
 LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
-endif
-
 include $(DLKM_DIR)/AndroidKernelModule.mk
