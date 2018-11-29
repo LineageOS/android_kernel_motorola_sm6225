4f16051c5974ca87d629c5ac529019ced185b2a3
Konstantin Makariev <hcv867@motorola.com>
2018-11-29 16:27:02 -0600

    ldc2114_mmi: report raw data through debugfs
    Replace character device with debugfs for reporting raw data.
    Multiple instances of driver and raw data reports supported.
    Source files moved to ldc2114_mmi folder to align with other
    drivers.
    
    Change-Id: I0357117962f5d492459091c28e8fe1cd403490a2
    Signed-off-by: Konstantin Makariev <hcv867@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1277626
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Konstantin Makariev <kmakariev@motorola.com>
    Submit-Approved: Jira Key

--- drivers/input/misc/ldc2114_mmi/Android.mk
+++ drivers/input/misc/ldc2114_mmi/Android.mk
@@ -6,5 +6,3 @@ LOCAL_MODULE := ldc2114_keys.ko
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
 include $(DLKM_DIR)/AndroidKernelModule.mk
-
-include $(call first-makefiles-under,$(LOCAL_PATH))
