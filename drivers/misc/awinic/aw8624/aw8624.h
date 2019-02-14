7016c7d9cf8a4dd6057054ebd9b326f0bdb7d4b9
Wen Xie <xiewen3@motorola.com>
2019-02-14 18:45:59 +0800

    haptic: revert update aw8624 vibrator driver
    Kernel panic:
    So revert "(CR): haptic: update aw8624 vibrator driver"
    
    This reverts commit e9324b897bee9f301f91d46e95401342c9410142.
    
    Change-Id: Ibc944b4a5a4427a13cc30384f94e376be75b0abd
    Reviewed-on: https://gerrit.mot.com/1308803
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key

--- drivers/misc/awinic/aw8624/aw8624.h
+++ drivers/misc/awinic/aw8624/aw8624.h
@@ -164,8 +164,6 @@ struct aw8624 {
 	struct device *dev;
 
 	struct mutex lock;
-	struct wake_lock wk_lock;
-	unsigned char wk_lock_flag;
 	struct hrtimer timer;
 	struct work_struct vibrator_work;
 	struct work_struct irq_work;
