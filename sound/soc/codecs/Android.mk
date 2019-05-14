44eda45bba352f035743bd949dac5f67877d3de8
huangys <huangys@motorola.com>
2019-05-14 17:06:50 +0800

    audio: remove Cirrus code to vendor/cirrus/audio/dlkm
    Cirrus has their own code release branch. and these branches are
    not compatible each other. audio team can't branch off motorola
    kernel modules
    
    Note: cs4012 and msm-cirrus-playback had not beeen moved away.
    
    Change-Id: Ib14bf3c5a5cfaa9c363a59c4ba0f871e11e9c92a
    Signed-off-by: huangys <huangys@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1350673
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Guobin Zhang <zhanggb@motorola.com>
    Submit-Approved: Jira Key

--- sound/soc/codecs/Android.mk
+++ sound/soc/codecs/Android.mk
@@ -2,59 +2,6 @@ DLKM_DIR := motorola/kernel/modules
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := cirrus_madera.ko
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
-include $(DLKM_DIR)/AndroidKernelModule.mk
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := cirrus_wm_adsp.ko
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
-include $(DLKM_DIR)/AndroidKernelModule.mk
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := cirrus_cs35l35.ko
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
-include $(DLKM_DIR)/AndroidKernelModule.mk
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := cirrus_cs35l36.ko
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
-include $(DLKM_DIR)/AndroidKernelModule.mk
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := cirrus_cs47l35.ko
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
-include $(DLKM_DIR)/AndroidKernelModule.mk
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := cirrus_cs47l90.ko
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
-include $(DLKM_DIR)/AndroidKernelModule.mk
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := aov_trigger.ko
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
-include $(DLKM_DIR)/AndroidKernelModule.mk
-
-include $(CLEAR_VARS)
-ifeq ($(CIRRUS_AMP_CODEC),cs35l41_spi)
-KERNEL_CFLAGS += CONFIG_SND_SOC_CS35L41_SPI=y
-else ifeq ($(CIRRUS_AMP_CODEC),cs35l41_i2c)
-KERNEL_CFLAGS += CONFIG_SND_SOC_CS35L41_I2C=y
-endif
-LOCAL_MODULE := cirrus_cs35l41.ko
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
-include $(DLKM_DIR)/AndroidKernelModule.mk
-
-include $(CLEAR_VARS)
 LOCAL_MODULE := cirrus_playback.ko
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
