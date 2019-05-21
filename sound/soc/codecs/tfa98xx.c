af366d4dc685e66c621dd1965a505cf73989093b
Yunfei Guo <guoyf2@motorola.com>
2019-05-21 02:37:22 -0500

    Revert "(CR): SurfNA: Add NXP smartpa log"
    This reverts commit 1e4c87d6ebc458a1f40a5b7dbff7d1fa9e1a1001.
    
    Change-Id: I6fa7f78125fdc72cd41d4ba35d96a04ebbb37c8c
    Reviewed-on: https://gerrit.mot.com/1357969
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Reviewed-by: Yunfei Guo <guoyf2@motorola.com>
    Submit-Approved: Jira Key

--- sound/soc/codecs/tfa98xx.c
+++ sound/soc/codecs/tfa98xx.c
@@ -1325,7 +1325,6 @@ static int tfa98xx_set_profile(struct snd_kcontrol *kcontrol,
 		return 0;
 
 	new_profile = ucontrol->value.integer.value[0];
-    pr_err("%s:new_profile:%d profile :%d\n",__func__,new_profile, profile);
 	if (new_profile == profile)
 		return 0;
 
@@ -1575,7 +1574,6 @@ static int tfa98xx_set_stereo_ctl(struct snd_kcontrol *kcontrol,
                                 tfa98xx->flags |= TFA98XX_FLAG_CHIP_SELECTED;
 				tfa98xx->profile = 1;
 				tfa98xx_mixer_profile = 1;
-                pr_err("%s:profile:%d\n",__func__,tfa98xx->profile);
 			}
                         else
                                 tfa98xx->flags &= ~TFA98XX_FLAG_CHIP_SELECTED;
@@ -1634,9 +1632,7 @@ static int tfa98xx_algo_set_gain_ctl(struct snd_kcontrol *kcontrol,
 
 	if(0 == tfa98xx_mixer_profile) {
 		ret = send_tfa_cal_in_band(&buff[1], nr - 1);
-	} else {
-        pr_err("%s:bypass mode current profile is %d\n", __func__, tfa98xx_mixer_profile);
-    }
+	}
 	return ret;
 }
 
@@ -2936,7 +2932,7 @@ static int tfa98xx_mute(struct snd_soc_dai *dai, int mute, int stream)
 			if(0 == tfa98xx->profile) {
 			    tfa98xx_adsp_send_calib_values(tfa98xx);
 			} else {
-			    pr_err("current profile:%d\n",tfa98xx->profile);
+			    pr_debug("current profile:%d\n",tfa98xx->profile);
 			}
 			/* Start DSP */
 			if ((tfa98xx->flags & TFA98XX_FLAG_CHIP_SELECTED) &&
