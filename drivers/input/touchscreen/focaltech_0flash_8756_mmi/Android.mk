5719007987885cfb9ebf39094e2fb203c634dc4d
wengjun1 <wengjun1@motorola.com>
2019-06-13 20:32:00 +0800

    Sofia: Sofiap: Focaltech ft8756 touch bringup
    - Initial bringup for ft8756
    - Will need to compatible with ft8719
    
    Change-Id: I6b5b2f68f219c44875d820b0450ecb41382f091d
    Signed-off-by: wengjun1 <wengjun1@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1370467
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Hujun Liao <liaohj@motorola.com>
    Submit-Approved: Jira Key
    (cherry picked from commit f8fdcedac1475f932c81ed8894fd4eb827f9649e)
    Reviewed-on: https://gerrit.mot.com/1372040

---
+++ drivers/input/touchscreen/focaltech_0flash_8756_mmi/Android.mk
@@ -0,0 +1,14 @@
+DLKM_DIR := motorola/kernel/modules
+LOCAL_PATH := $(call my-dir)
+
+ifneq ($(FOCALTECH_TOUCH_IC_NAME),)
+	KERNEL_CFLAGS += CONFIG_INPUT_FOCALTECH_0FLASH_MMI_IC_NAME=$(FOCALTECH_TOUCH_IC_NAME)
+else
+	KERNEL_CFLAGS += CONFIG_INPUT_FOCALTECH_0FLASH_MMI_IC_NAME=ft8719
+endif
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := focaltech_0flash_8756_mmi.ko
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
+include $(DLKM_DIR)/AndroidKernelModule.mk
