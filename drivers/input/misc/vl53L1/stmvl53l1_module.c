94a1afa29054d44c59f00830ad640a122ac91304
liuyc20 <liuyc20@lenovo.com>
2019-07-01 16:09:35 +0800

    vl53l1: fix build error
    loff_t --> %llu
    size_t --> %lu
    
    Change-Id: I038cad8baac70c280a591f358fd7a4a1bbd55761
    Signed-off-by: liuyc20 <liuyc20@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/1379385
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Reviewed-by: Xiaojun Ji <jixj@motorola.com>
    Reviewed-by: Dawei Wang <wangdw10@motorola.com>
    Submit-Approved: Jira Key

--- drivers/input/misc/vl53L1/stmvl53l1_module.c
+++ drivers/input/misc/vl53L1/stmvl53l1_module.c
@@ -2299,7 +2299,7 @@ static ssize_t stmvl53l1_calib_data_read(struct file *filp,
 
 	mutex_lock(&data->work_mutex);
 
-	vl53l1_dbgmsg("off = %lld / count = %d", off, count);
+	vl53l1_dbgmsg("off = %llu / count = %lu", off, count);
 
 	/* sanity check */
 	if (off < 0 || off > sizeof(VL53L1_CalibrationData_t))
@@ -2344,7 +2344,7 @@ static ssize_t stmvl53l1_calib_data_write(struct file *filp,
 
 	mutex_lock(&data->work_mutex);
 
-	vl53l1_dbgmsg("off = %lld / count = %d", off, count);
+	vl53l1_dbgmsg("off = %llu / count = %lu", off, count);
 
 	if (data->enable_sensor) {
 		rc = -EBUSY;
@@ -2400,7 +2400,7 @@ static ssize_t stmvl53l1_zone_calib_data_read(struct file *filp,
 
 	mutex_lock(&data->work_mutex);
 
-	vl53l1_dbgmsg("off = %lld / count = %d", off, count);
+	vl53l1_dbgmsg("off = %llu / count = %lu", off, count);
 
 	/* sanity check */
 	if (off < 0 || off > sizeof(stmvl531_zone_calibration_data_t))
@@ -2447,7 +2447,7 @@ static ssize_t stmvl53l1_zone_calib_data_write(struct file *filp,
 
 	mutex_lock(&data->work_mutex);
 
-	vl53l1_dbgmsg("off = %lld / count = %d", off, count);
+	vl53l1_dbgmsg("off = %llu / count = %lu", off, count);
 
 	/* implementation if quite fragile. We suppose successive access. We
 	 * trigger set on last byte write if amount is exact.
