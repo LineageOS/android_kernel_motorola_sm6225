63ee99116bd49fd33849264d4e76d67f3900d354
Bang Nguyen <bangnguyen@motorola.com>
2019-07-24 14:31:35 -0500

    disp: msm: dsi: Add "bl_ctrl_dummy" to panel bl type
    Add a new display's backlight type "bl_ctrl_dummy". With this
    backlight type, the panel backlight driver will ignore any backlight
    requests. This new backlight type will be used with a bare_board or
    no-display configuration and the display driver doesn't need to have
    any communication with panel.
    
    Port from: cfb2acd
    
    Change-Id: I2108d7477009d06ac350ca49c380b64c9d004f65
    Signed-off-by: Bang Nguyen <bangnguyen@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1442424
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Submit-Approved: Jira Key
    Reviewed-on: https://gerrit.mot.com/1509792
    Reviewed-by: Hua Tan <tanhua1@motorola.com>

--- msm/dsi/dsi_panel.c
+++ msm/dsi/dsi_panel.c
@@ -754,6 +754,9 @@ int dsi_panel_set_backlight(struct dsi_panel *panel, u32 bl_lvl)
 	case DSI_BACKLIGHT_DCS:
 		rc = dsi_panel_update_backlight(panel, bl_lvl);
 		break;
+	case DSI_BACKLIGHT_DUMMY:
+		rc = 0;
+		break;
 	case DSI_BACKLIGHT_EXTERNAL:
 		break;
 	case DSI_BACKLIGHT_PWM:
@@ -833,6 +836,8 @@ static int dsi_panel_bl_register(struct dsi_panel *panel)
 		break;
 	case DSI_BACKLIGHT_DCS:
 		break;
+	case DSI_BACKLIGHT_DUMMY:
+		break;
 	case DSI_BACKLIGHT_EXTERNAL:
 		break;
 	case DSI_BACKLIGHT_PWM:
@@ -867,6 +872,8 @@ static int dsi_panel_bl_unregister(struct dsi_panel *panel)
 	case DSI_BACKLIGHT_WLED:
 		break;
 	case DSI_BACKLIGHT_DCS:
+		break;
+	case DSI_BACKLIGHT_DUMMY:
 		break;
 	case DSI_BACKLIGHT_EXTERNAL:
 		break;
@@ -2360,6 +2367,8 @@ static int dsi_panel_parse_bl_config(struct dsi_panel *panel)
 		panel->bl_config.type = DSI_BACKLIGHT_WLED;
 	} else if (!strcmp(bl_type, "bl_ctrl_dcs")) {
 		panel->bl_config.type = DSI_BACKLIGHT_DCS;
+	} else if (!strcmp(bl_type, "bl_ctrl_dummy")) {
+		panel->bl_config.type = DSI_BACKLIGHT_DUMMY;
 	} else if (!strcmp(bl_type, "bl_ctrl_external")) {
 		panel->bl_config.type = DSI_BACKLIGHT_EXTERNAL;
 	} else {
