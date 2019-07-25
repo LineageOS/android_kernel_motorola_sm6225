c4ac8db3fe6054de50da5f7917c6d75eedc7cc06
Bang Nguyen <bangnguyen@motorola.com>
2019-07-25 10:11:43 -0500

    disp: msm: dsi: add prints for DSI/panel turning off/on
    Add some pr_info to indicate the DSI and panel are turning off
    and on for ESD or black screen investigation.
    
    Port from: 6fcdea6
    Signed-off-by: Bang Nguyen <bangnguyen@motorola.com>
    
    Change-Id: I334890799fd18be9951b8d7579717d362c9878c3
    Reviewed-on: https://gerrit.mot.com/1444391
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Bang Nguyen <bangnguyen@motorola.com>
    Reviewed-by: Shuo Yan <shuoyan@motorola.com>
    Submit-Approved: Jira Key
    Reviewed-on: https://gerrit.mot.com/1509793
    Reviewed-by: Hua Tan <tanhua1@motorola.com>

--- msm/dsi/dsi_display.c
+++ msm/dsi/dsi_display.c
@@ -7155,6 +7155,7 @@ int dsi_display_prepare(struct dsi_display *display)
 		return -EINVAL;
 	}
 
+	DSI_INFO("%s(%s)+\n", __func__, display->drm_conn->name);
 	SDE_EVT32(SDE_EVTLOG_FUNC_ENTRY);
 	mutex_lock(&display->display_lock);
 
@@ -7778,6 +7779,7 @@ int dsi_display_disable(struct dsi_display *display)
 		return -EINVAL;
 	}
 
+	DSI_INFO("%s(%s)+\n", __func__, display->drm_conn->name);
 	SDE_EVT32(SDE_EVTLOG_FUNC_ENTRY);
 	mutex_lock(&display->display_lock);
 
