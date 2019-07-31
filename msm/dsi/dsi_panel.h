fa247f6d83657fb0380a56f74c044fae0994b4d7
Bang Nguyen <bangnguyen@motorola.com>
2019-07-31 12:58:34 -0500

    disp: msm: add panel's param feature to display FW
    Add the display's param feature such as HBM, ACL, or CABC feature to
    display framework
    
    Port from: f988eb8
    
    Change-Id: I445ea7e58188d2354888e2202ed7119bf2e55fc9
    Signed-off-by: Bang Nguyen <bangnguyen@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1444400
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Submit-Approved: Jira Key
    Reviewed-on: https://gerrit.mot.com/1509802
    Reviewed-by: Hua Tan <tanhua1@motorola.com>

--- msm/dsi/dsi_panel.h
+++ msm/dsi/dsi_panel.h
@@ -28,7 +28,6 @@
 
 #define DSI_MODE_MAX 32
 
-#define DSI_PANEL_MAX_PANEL_LEN 256
 
 /*
  * Defining custom dsi msg flag,
@@ -36,6 +35,9 @@
  * Override to use async transfer
  */
 #define MIPI_DSI_MSG_ASYNC_OVERRIDE BIT(4)
+
+#define DSI_PANEL_MAX_PANEL_LEN	256
+#define MAX_PARAM_NAME 10
 
 enum dsi_panel_rotation {
 	DSI_PANEL_ROTATE_NONE = 0,
@@ -176,6 +178,27 @@ struct drm_panel_esd_config {
 	u32 groups;
 };
 
+enum hbm_state {
+	HBM_OFF_STATE = 0,
+	HBM_ON_STATE,
+	HBM_STATE_NUM
+};
+
+struct panel_param_val_map {
+	int state;
+	enum dsi_cmd_set_type type;
+	struct dsi_panel_cmd_set *cmds;
+};
+
+struct panel_param {
+	const char *param_name;
+	struct panel_param_val_map *val_map;
+	const u16 val_max;
+	const u16 default_value;
+	u16 value;
+	bool is_supported;
+};
+
 struct dsi_panel {
 	const char *name;
 	const char *type;
@@ -236,6 +259,8 @@ struct dsi_panel {
 
 	u32 disp_on_chk_val;
 	bool no_panel_on_read_support;
+
+	struct panel_param *param_cmds;
 };
 
 static inline bool dsi_panel_ulps_feature_enabled(struct dsi_panel *panel)
@@ -355,5 +380,8 @@ void dsi_panel_ext_bridge_put(struct dsi_panel *panel);
 
 void dsi_panel_calc_dsi_transfer_time(struct dsi_host_common_cfg *config,
 		struct dsi_display_mode *mode, u32 frame_threshold_us);
+
+int dsi_panel_set_param(struct dsi_panel *panel,
+			struct msm_param_info *param_info);
 
 #endif /* _DSI_PANEL_H_ */
