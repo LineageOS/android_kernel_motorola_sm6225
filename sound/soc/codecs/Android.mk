720af089a66ca7e516a594df0c4d0ad2ecfce139
liaohj <liaohj@lenovo.com>
2019-08-05 10:21:41 +0800

    sound/codecs:add ti2558 driver
    sofiar use smart pa ti2558,
    add kernel drivers.
    
    Change-Id: I02b490c17e96224d2fd9510d3e214900a4172b4a
    Reviewed-on: https://gerrit.mot.com/1396120
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Yuesheng Huang <huangys@motorola.com>
    Reviewed-by: Hujun Liao <liaohj@motorola.com>
    Submit-Approved: Jira Key

--- sound/soc/codecs/Android.mk
+++ sound/soc/codecs/Android.mk
@@ -24,3 +24,9 @@ LOCAL_MODULE := tas2560_calib.ko
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
 include $(DLKM_DIR)/AndroidKernelModule.mk
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := tas2562.ko
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
+include $(DLKM_DIR)/AndroidKernelModule.mk
