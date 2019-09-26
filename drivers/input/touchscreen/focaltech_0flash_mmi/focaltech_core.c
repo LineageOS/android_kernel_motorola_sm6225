ec353b493017b5a065b10a2ba144a730f40a931a
haojl2 <haojl2@motorola.com>
2019-09-26 11:45:20 +0800

    focaltech_0flash_mmi: Change sensor flag into on-change
    TP:
    Change sensor flag into on-change sensor.
    one shot sensor can not work normal for TDDI panel
    
    Change-Id: I0a33ad017b2a11e86f3f437009904678509a9f22
    Signed-off-by: haojl2 <haojl2@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1435389
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Jianlin Nie <niejl1@motorola.com>
    Reviewed-by: Hujun Liao <liaohj@motorola.com>
    Submit-Approved: Jira Key

--- drivers/input/touchscreen/focaltech_0flash_mmi/focaltech_core.c
+++ drivers/input/touchscreen/focaltech_0flash_mmi/focaltech_core.c
@@ -2059,6 +2059,7 @@ static int _fts_ts_suspend(struct device *dev)
             ts_data->suspended = true;
 #ifdef FOCALTECH_SENSOR_EN
             ts_data->screen_state = SCREEN_OFF;
+            ts_data->wakeable = true;
             mutex_unlock(&ts_data->state_mutex);
             FTS_INFO("Enter gesture suspend mode.");
 #endif
@@ -2165,12 +2166,13 @@ static int _fts_ts_resume(struct device *dev)
 #if FTS_GESTURE_EN
 
 #ifdef FOCALTECH_SENSOR_EN
-    if (ts_data->should_enable_gesture) {
+    if (ts_data->wakeable) {
 #endif
         if (fts_gesture_resume(ts_data) == 0) {
             ts_data->suspended = false;
 #ifdef FOCALTECH_SENSOR_EN
             ts_data->screen_state = SCREEN_ON;
+            ts_data->wakeable = false;
             mutex_unlock(&ts_data->state_mutex);
             FTS_INFO("Exit from gesture suspend mode.");
 #endif
