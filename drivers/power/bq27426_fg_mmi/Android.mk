0de52552835d61595800fc615a9e9b81794e015e
cmsantos <cmsantos@motorola.com>
2019-10-31 21:22:46 -0500

    power: bq_27426_mmi: FG driver port
    First port of BQ27426 FG driver, based on Defender project.
    This version handles two different FG IC's.
    
    Change-Id: I0f06b90ffafd35780c067d995d9a7738d48db567
    Signed-off-by: cmsantos <cmsantos@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1447519
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Ling Jin <lingjin@motorola.com>
    Submit-Approved: Jira Key

---
+++ drivers/power/bq27426_fg_mmi/Android.mk
@@ -0,0 +1,22 @@
+DLKM_DIR := motorola/kernel/modules
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := bq27426_fg_mmi.ko
+LOCAL_MODULE_TAGS := optional
+
+ifeq ($(ATL_4000MAH_8A_BATTERY_PROFILE),true)
+	KERNEL_CFLAGS += CONFIG_ATL_4000MAH_8A_BATTERY_PROFILE=y
+endif
+
+ifeq ($(SCUD_4000MAH_8A_BATTERY_PROFILE),true)
+	KERNEL_CFLAGS += CONFIG_SCUD_4000MAH_8A_BATTERY_PROFILE=y
+endif
+
+ifeq ($(DLKM_INSTALL_TO_VENDOR_OUT),true)
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR)/lib/modules/
+else
+LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
+endif
+
+include $(DLKM_DIR)/AndroidKernelModule.mk
