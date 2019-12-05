486cf61889e9ee1c998cd56e2bb6b55e27dc3cf5
Bang Nguyen <bangnguyen@motorola.com>
2019-12-05 15:09:26 -0600

    disp: msm: Restore old backlight setting after exitting HBM
    The backlight and HBM are using the same command 0x51. The backlight
    ranger will be from 0x000 to 0x640 while HBM on will be at 0xfff and
    HBM off will be at 0x640. This is why the display driver should
    restore the last backlight setting after exiting the HBM mode.
    
    Change-Id: I23b9e190924d13c9cdf50a6543033647395a8cf6
    Signed-off-by: daiss1 <daiss1@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1469387
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Guobin Zhang <zhanggb@motorola.com>
    Submit-Approved: Jira Key
    Reviewed-by: Bang Nguyen <bangnguyen@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1509825
    Reviewed-by: Hua Tan <tanhua1@motorola.com>

--- msm/dsi/dsi_panel.c
+++ msm/dsi/dsi_panel.c
@@ -694,11 +694,14 @@ static int dsi_panel_update_backlight(struct dsi_panel *panel,
 	else
 		rc = mipi_dsi_dcs_set_display_brightness(dsi, bl_lvl);
 
-	if (panel->is_hbm_using_51_cmd && panel->is_hbm_on) {
+	if (panel->is_hbm_using_51_cmd) {
 		panel->bl_lvl_during_hbm = bl_lvl;
-		DSI_DEBUG("HBM is on.. ignore setting backlight. bl_vl=%d\n",
+
+		if (panel->is_hbm_on) {
+			DSI_DEBUG("HBM is on.. ignore setting backlight. bl_vl=%d\n",
 						panel->bl_lvl_during_hbm);
-		return 0;
+			return 0;
+		}
 	}
 
 	if (bl->bl_2bytes_enable)
