e1137435d59ab1922842e07aa4b441dc5a7a88d4
haojl2 <haojl2@motorola.com>
2019-12-11 17:36:18 +0800

    sec_mmi: register moto touch class.
    TP:
    Register touchscreen_mmi to support firmware update, get panel and
    ic info.
    
    Change-Id: I1ff872092375918590e8d977ff61815eb5391f15
    Signed-off-by: haojl2 <haojl2@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1469544
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Jianqi Yang <yangj@motorola.com>
    Submit-Approved: Jira Key

--- drivers/input/touchscreen/sec_mmi/Android.mk
+++ drivers/input/touchscreen/sec_mmi/Android.mk
@@ -1,6 +1,10 @@
 DLKM_DIR := motorola/kernel/modules
 LOCAL_PATH := $(call my-dir)
 
+ifneq ($(findstring touchscreen_mmi.ko,$(BOARD_VENDOR_KERNEL_MODULES)),)
+	KERNEL_CFLAGS += CONFIG_INPUT_TOUCHSCREEN_MMI=y
+endif
+
 include $(CLEAR_VARS)
 LOCAL_MODULE := sec_mmi.ko
 LOCAL_MODULE_TAGS := optional
