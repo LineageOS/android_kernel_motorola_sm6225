aec6b629186721b6b8e85b5fb3d711ef2d4029cd
zhouwl <zhouwl@lenovo.com>
2019-12-26 18:47:45 +0800

    ASoC: add Awinic aw882xx driver
    Astro uses aw882xx chip for driving loudspeaker,
    add its driver.
    
    Change-Id: I28273397535050ce50a62ea14d14bb71749fb696
    Signed-off-by: zhouwl <zhouwl@mt.com>
    Reviewed-on: https://gerrit.mot.com/1476953
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Hongshu Lou <louhs1@motorola.com>
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Submit-Approved: Jira Key

--- sound/soc/codecs/Android.mk
+++ sound/soc/codecs/Android.mk
@@ -30,3 +30,9 @@ LOCAL_MODULE := tas2562.ko
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
 include $(DLKM_DIR)/AndroidKernelModule.mk
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := snd_smartpa_aw882xx.ko
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
+include $(DLKM_DIR)/AndroidKernelModule.mk
