9abf587387c02bd2d481a6cb0e348761a52b1ba9
Konstantin Makariev <hcv867@motorola.com>
2020-01-03 15:29:47 -0600

    synaptics_mmi: add touchscreen_mmi class check
    This DLKM will only be built if touchscreen_mmi class is not used
    for backward compatibility.
    
    Change-Id: Iced4457c6f87faa726ea6de44f9f625955d646fb
    Signed-off-by: Konstantin Makariev <hcv867@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1479387
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Konstantin Makariev <kmakariev@motorola.com>
    Submit-Approved: Jira Key

--- drivers/input/touchscreen/synaptics_mmi/Android.mk
+++ drivers/input/touchscreen/synaptics_mmi/Android.mk
@@ -1,6 +1,8 @@
 DLKM_DIR := motorola/kernel/modules
 LOCAL_PATH := $(call my-dir)
 
+ifeq ($(findstring touchscreen_mmi.ko,$(BOARD_VENDOR_KERNEL_MODULES)),)
+
 include $(CLEAR_VARS)
 LOCAL_MODULE := synaptics_mmi.ko
 LOCAL_MODULE_TAGS := optional
@@ -24,3 +26,5 @@ LOCAL_MODULE := synaptics_dsx_test_reporting.ko
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
 include $(DLKM_DIR)/AndroidKernelModule.mk
+
+endif
