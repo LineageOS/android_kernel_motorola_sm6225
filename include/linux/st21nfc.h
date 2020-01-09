11f64b57f3859e30c1910eccd873bc90bf89590b
Ningkai Zhuang <zhuangnk1@lenovo.com>
2020-01-09 11:09:28 +0800

    Update driver version to 2.0.4
    Support new ioctl command and compatibale with old one
    Add interrupt control to solve int can not wake up system issue
    
    Change-Id: I3229f343c359ec6075739ea3e2d30554b07052a4
    Signed-off-by: Ningkai Zhuang <zhuangnk1@mt.com>
    Reviewed-on: https://gerrit.mot.com/1481162
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Tao Sun <suntao2@motorola.com>
    Submit-Approved: Jira Key

--- include/linux/st21nfc.h
+++ include/linux/st21nfc.h
@@ -25,13 +25,27 @@
  * ST21NFC power control via ioctl
  * ST21NFC_GET_WAKEUP :  poll gpio-level for Wakeup pin
  */
-#define ST21NFC_GET_WAKEUP	      _IOR(ST21NFC_MAGIC, 0x01, unsigned int)
-#define ST21NFC_PULSE_RESET		_IOR(ST21NFC_MAGIC, 0x02, unsigned int)
-#define ST21NFC_SET_POLARITY_RISING   _IOR(ST21NFC_MAGIC, 0x03, unsigned int)
-#define ST21NFC_SET_POLARITY_HIGH     _IOR(ST21NFC_MAGIC, 0x05, unsigned int)
-#define ST21NFC_GET_POLARITY	      _IOR(ST21NFC_MAGIC, 0x07, unsigned int)
-#define ST21NFC_RECOVERY              _IOR(ST21NFC_MAGIC, 0x08, unsigned int)
+#define ST21NFC_GET_WAKEUP _IO(ST21NFC_MAGIC, 0x01)
+#define ST21NFC_PULSE_RESET _IO(ST21NFC_MAGIC, 0x02)
+#define ST21NFC_SET_POLARITY_RISING _IO(ST21NFC_MAGIC, 0x03)
+#define ST21NFC_SET_POLARITY_FALLING _IO(ST21NFC_MAGIC, 0x04)
+#define ST21NFC_SET_POLARITY_HIGH _IO(ST21NFC_MAGIC, 0x05)
+#define ST21NFC_SET_POLARITY_LOW _IO(ST21NFC_MAGIC, 0x06)
+#define ST21NFC_GET_POLARITY _IO(ST21NFC_MAGIC, 0x07)
+#define ST21NFC_RECOVERY _IO(ST21NFC_MAGIC, 0x08)
+#define ST21NFC_USE_ESE _IOW(ST21NFC_MAGIC, 0x09, unsigned int)
 
+// Keep compatibility with older user applications.
+#define ST21NFC_LEGACY_GET_WAKEUP _IOR(ST21NFC_MAGIC, 0x01, unsigned int)
+#define ST21NFC_LEGACY_PULSE_RESET _IOR(ST21NFC_MAGIC, 0x02, unsigned int)
+#define ST21NFC_LEGACY_SET_POLARITY_RISING _IOR(ST21NFC_MAGIC, 0x03, \
+	unsigned int)
+#define ST21NFC_LEGACY_SET_POLARITY_FALLING _IOR(ST21NFC_MAGIC, 0x04, \
+	unsigned int)
+#define ST21NFC_LEGACY_SET_POLARITY_HIGH _IOR(ST21NFC_MAGIC, 0x05, unsigned int)
+#define ST21NFC_LEGACY_SET_POLARITY_LOW _IOR(ST21NFC_MAGIC, 0x06, unsigned int)
+#define ST21NFC_LEGACY_GET_POLARITY _IOR(ST21NFC_MAGIC, 0x07, unsigned int)
+#define ST21NFC_LEGACY_RECOVERY _IOR(ST21NFC_MAGIC, 0x08, unsigned int)
 
 struct st21nfc_platform_data {
 	unsigned int irq_gpio;
@@ -40,3 +54,10 @@ struct st21nfc_platform_data {
 	unsigned int clkreq_gpio;
 	unsigned int polarity_mode;
 };
+
+#define ST54SPI_CB_RESET_END 0
+#define ST54SPI_CB_RESET_START 1
+#define ST54SPI_CB_ESE_NOT_USED 2
+#define ST54SPI_CB_ESE_USED 3
+void st21nfc_register_st54spi_cb(void (*cb)(int, void *), void *data);
+void st21nfc_unregister_st54spi_cb(void);
