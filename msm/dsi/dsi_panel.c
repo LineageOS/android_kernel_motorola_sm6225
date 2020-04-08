2f9fb7155a90acd6f9d45081e9517d3418bed5d5
houdz1 <houdz1@lenovo.com>
2020-04-08 14:19:27 +0800

    drm/msm/dsi-staGing: add default brightness property
    Add a new property to specify the default brightness level that would
    set for usecases where no explicit brightness level is configured.
    Mot-CRs-fixed: (CR)
    
    Change-Id: Iecc6f4e0b5b9fbda5d8bf97efa7d4de4dbbd5570
    Signed-off-by: houdz1 <houdz1@mt.com>
    Reviewed-on: https://gerrit.mot.com/1697641
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Guobin Zhang <zhanggb@motorola.com>
    Submit-Approved: Jira Key
    Signed-off-by: tanhua1 <tanhua1@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2186536
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>

--- msm/dsi/dsi_panel.c
+++ msm/dsi/dsi_panel.c
@@ -3069,7 +3069,6 @@ static int dsi_panel_parse_bl_config(struct dsi_panel *panel)
 	panel->bl_config.bl_inverted_dbv = utils->read_bool(utils->data,
 		"qcom,mdss-dsi-bl-inverted-dbv");
 
-
 	rc = utils->read_u32(utils->data, "qcom,mdss-dsi-bl-level-align", &val);
 	if (rc) {
 		DSI_DEBUG("[%s] bl_level_align unspecified, defaulting to normal, no special algin\n",
@@ -3077,6 +3076,15 @@ static int dsi_panel_parse_bl_config(struct dsi_panel *panel)
 		panel->bl_config.bl_level_align = DSI_BACKLIGHT_LEVEL_ALIGN_NORMAL;
 	} else {
 		panel->bl_config.bl_level_align = val;
+	}
+
+	rc = utils->read_u32(utils->data, "qcom,mdss-dsi-bl-default-level", &val);
+	if (rc) {
+		panel->bl_config.brightness_default_level =
+			panel->bl_config.brightness_max_level;
+		pr_debug("set default brightness to max level\n");
+	} else {
+		panel->bl_config.brightness_default_level = val;
 	}
 
 	panel->bl_config.bl_2bytes_enable = utils->read_bool(utils->data,
