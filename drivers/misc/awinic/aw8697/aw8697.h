1dd38c421222036b669f5d3f61713fbe57efe58a
yangjq <yangjq@lenovo.com>
2020-06-11 18:00:42 +0530

    aw8697: Support repeatedly waveform playing
    Play those waveforms repeatedly with wave id higher than 200;
    Stop the repeatedly playing after 2 minutes automatically to avoid
    the problem when no stop command is received as expected;
    
    As for the Code-Review-1 of missing propagation to branch(es):
    bq-shm2018
    
    NO_PROP_NEEDED: This propagation is only needed when the change of
    framework for this new feature is integrated, being done by engineers
    in charge of that product.
    
    Change-Id: Ic9201ffbe726de66a513b6ea501b4e3bf99beef1
    Signed-off-by: yangjq <yangjq@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/1577403
    Reviewed-by: Jialei Hao <haojl2@motorola.com>
    Reviewed-by: Jianqi Yang <yangj@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1611551
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Ling Jin <lingjin@motorola.com>
    Submit-Approved: Jira Key

--- drivers/misc/awinic/aw8697/aw8697.h
+++ drivers/misc/awinic/aw8697/aw8697.h
@@ -212,6 +212,7 @@ enum aw8697_haptic_mode{
 	HAPTIC_SHORT	= 0x01,
 	HAPTIC_LONG	= 0x02,
 	HAPTIC_RTP	= 0x03,
+	HAPTIC_RTP_LOOP	= 0x04,
 };
 /*********************************************************
  *
