f50ae08070e125fd1e3659b9a1a2f616986d7452
litong <litong20@lenovo.com>
2020-07-23 16:16:42 +0800

    Initial goodix_berlin touchscreen driver code
    TP:
    Initial goodix_berlin touchscreen driver code V1.0.1.
    
    NO_PROP_NEEDED: <the driver code is for Viking project>
    
    Change-Id: I872624045aea763c0483abb561dd7e3765544cd8
    Reviewed-on: https://gerrit.mot.com/1702702
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Jichao Zou <zoujc@motorola.com>
    Submit-Approved: Jira Key

---
+++ drivers/input/touchscreen/goodix_berlin_mmi/Android.mk
@@ -0,0 +1,20 @@
+DLKM_DIR := motorola/kernel/modules
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := goodix_brl_mmi.ko
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
+include $(DLKM_DIR)/AndroidKernelModule.mk
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := goodix_brl_tools_mmi.ko
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
+include $(DLKM_DIR)/AndroidKernelModule.mk
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := goodix_brl_gesture_mmi.ko
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
+include $(DLKM_DIR)/AndroidKernelModule.mk
