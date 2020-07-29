a2b5a943948d002119f8531250ddfdf236208254
yanyh2 <yanyh2@motorola.com>
2020-07-29 20:49:01 +0800

    vl53l5: add stmvl53l5 init and deinit functions
    Add init and deinit functions to prepare power and IO pins
    for module init and exit.
    
    Change-Id: Iaf8d26409f71998bb52b63873237198adef7a0cb
    Signed-off-by: yanyh2 <yanyh2@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1708657
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Jianqi Yang <yangj@motorola.com>
    Reviewed-by: Yuecai Liu <liuyc20@motorola.com>
    Submit-Approved: Jira Key

--- drivers/input/misc/vl53L5/stmvl53l5_module.c
+++ drivers/input/misc/vl53L5/stmvl53l5_module.c
@@ -45,6 +45,7 @@
 #include <linux/platform_device.h>
 #include <linux/version.h>
 #include <linux/gpio.h>
+#include <linux/regulator/consumer.h>
 
 #include "stmvl53l5_i2c.h"
 #include "stmvl53l5_spi.h"
@@ -72,6 +73,14 @@ static uint8_t spi_driver_registered = 0;
 static uint8_t misc_registered = 0;
 
 static struct spi_data_t spi_data;
+
+static struct regulator *vl53l5_avdd = NULL;
+static struct regulator *vl53l5_iovdd = NULL;
+struct stmvl53l5_pinctrl_info {
+	struct pinctrl *pinctrl;
+	struct pinctrl_state *gpio_state_active;
+	struct pinctrl_state *gpio_state_suspend;
+} vl53l5_pinctrl_info = {NULL, NULL, NULL};
 
 // ------- i2c ---------------------------
 static const struct i2c_device_id stmvl53l5_i2c_id[] = {
@@ -258,6 +267,94 @@ static const struct file_operations stmvl53l5_ranging_fops = {
 	.release 		= stmvl53l5_release,
 };
 
+static int stmvl53l5_init(struct device *dev)
+{
+	int ret = 0;
+
+	vl53l5_pinctrl_info.pinctrl = devm_pinctrl_get(dev);
+	if (!IS_ERR_OR_NULL(vl53l5_pinctrl_info.pinctrl)) {
+		vl53l5_pinctrl_info.gpio_state_active =
+			pinctrl_lookup_state(vl53l5_pinctrl_info.pinctrl, "laser_default");
+		vl53l5_pinctrl_info.gpio_state_suspend =
+			pinctrl_lookup_state(vl53l5_pinctrl_info.pinctrl, "laser_suspend");
+		pinctrl_select_state(vl53l5_pinctrl_info.pinctrl, vl53l5_pinctrl_info.gpio_state_active);
+	} else {
+		dev_err(dev, "stmvl53l5: Error. Getting pinctrl handle failed\n");
+	}
+
+	if (vl53l5_avdd || vl53l5_iovdd) {
+		dev_warn(dev, "stmvl53l5: Power regulator already on\n");
+		return 0;
+	}
+
+	vl53l5_avdd = devm_regulator_get_optional(dev, "avdd-vl53l5");
+	if (IS_ERR(vl53l5_avdd) || vl53l5_avdd == NULL) {
+		dev_err(dev, "stmvl53l5: Error. Could not get avdd-vl53l5 power regulator\n");
+		ret = -EINVAL;
+		goto avdd_fail;
+	}
+
+	vl53l5_iovdd = devm_regulator_get_optional(dev, "iovdd-vl53l5");
+	if (IS_ERR(vl53l5_iovdd) || vl53l5_iovdd == NULL) {
+		dev_err(dev, "stmvl53l5: Error. Could not get iovdd-vl53l5 power regulator\n");
+		ret = -EINVAL;
+		goto iovdd_fail;
+	}
+
+	ret = regulator_enable(vl53l5_avdd);
+	if (ret) {
+		dev_err(dev, "stmvl53l5: Error. Could not enable avdd-vl53l5 power regulator\n");
+		goto avdd_en_fail;
+	}
+	ret = regulator_enable(vl53l5_iovdd);
+	if (ret) {
+		dev_err(dev, "stmvl53l5: Error. Could not enable iovdd-vl53l5 power regulator\n");
+		goto iovdd_en_fail;
+	}
+
+	return 0;
+
+iovdd_en_fail:
+	regulator_disable(vl53l5_avdd);
+avdd_en_fail:
+	devm_regulator_put(vl53l5_iovdd);
+iovdd_fail:
+	devm_regulator_put(vl53l5_avdd);
+avdd_fail:
+	vl53l5_avdd = NULL;
+	vl53l5_iovdd = NULL;
+
+	return ret;
+}
+
+static int stmvl53l5_deinit(struct device *dev)
+{
+	int ret = 0;
+
+	if (!vl53l5_avdd || !vl53l5_iovdd) {
+		dev_err(dev, "stmvl53l5: Error. Power regulator already off\n");
+		return 0;
+	}
+
+	ret = regulator_disable(vl53l5_avdd);
+	if (ret) {
+		dev_err(dev, "stmvl53l5: Error. Could not disable avdd-vl53l5 power regulator\n");
+	}
+	ret = regulator_disable(vl53l5_iovdd);
+	if (ret) {
+		dev_err(dev, "stmvl53l5: Error. Could not disable iovdd-vl53l5 power regulator\n");
+	}
+
+	devm_regulator_put(vl53l5_iovdd);
+	devm_regulator_put(vl53l5_avdd);
+	vl53l5_avdd = NULL;
+	vl53l5_iovdd = NULL;
+
+	if (!IS_ERR_OR_NULL(vl53l5_pinctrl_info.pinctrl))
+		pinctrl_select_state(vl53l5_pinctrl_info.pinctrl, vl53l5_pinctrl_info.gpio_state_suspend);
+
+	return ret;
+}
 
 static int stmvl53l5_i2c_probe(struct i2c_client *client,
 				const struct i2c_device_id *id)
@@ -266,13 +363,21 @@ static int stmvl53l5_i2c_probe(struct i2c_client *client,
 	uint8_t page = 0, revision_id = 0, device_id = 0;
 	stmvl53l5_i2c_client = client;
 
-	i2c_not_spi = 1;
-
 	printk("stmvl53l5: probing i2c\n");
 
+	ret = stmvl53l5_init(&client->dev);
+	if (ret) {
+		pr_err("stmvl53l5: Error. Power up failed, ret = %d\n", ret);
+		return ret;
+	}
+
+	i2c_not_spi = 1;
+
 	raw_data_buffer = kzalloc(VL53L5_COMMS_CHUNK_SIZE, GFP_DMA | GFP_KERNEL);
-	if (raw_data_buffer == NULL)
-		 return -ENOMEM;
+	if (raw_data_buffer == NULL) {
+		 ret = -ENOMEM;
+		goto exit;
+	}
 
 	ret = stmvl53l5_write_multi(client, raw_data_buffer, 0x7FFF, &page, 1);
 	ret |= stmvl53l5_read_multi(client, raw_data_buffer, 0x00, &device_id, 1);
@@ -280,7 +385,8 @@ static int stmvl53l5_i2c_probe(struct i2c_client *client,
 
 	if ((device_id != 0xF0) || (revision_id != 0x02)) {
 		pr_err("stmvl53l5: Error. Could not read device and revision id registers\n");
-		return ret;
+		ret = ret? ret : -ENXIO;
+		goto exit;
 	}
 	printk("stmvl53l5: device_id : 0x%x. revision_id : 0x%x\n", device_id, revision_id);
 
@@ -292,7 +398,7 @@ static int stmvl53l5_i2c_probe(struct i2c_client *client,
 	ret = misc_register(&st_tof_miscdev);
 	if (ret) {
 		pr_err("stmvl53l5 : Failed to create misc device, err = %d\n", ret);
-		return -1;
+		goto exit;
 	}
 
 	misc_registered = 1;
@@ -307,6 +413,18 @@ static int stmvl53l5_i2c_probe(struct i2c_client *client,
 		pr_err("stmvl53l5 : could not move the device to low power = %d\n", ret);
 	}
 
+	if (ret)
+		goto exit;
+
+	return 0;
+
+exit:
+	if (raw_data_buffer) {
+		kfree(raw_data_buffer);
+		raw_data_buffer = NULL;
+	}
+	stmvl53l5_deinit(&client->dev);
+
 	return ret;
 }
 
@@ -315,6 +433,9 @@ static int stmvl53l5_i2c_remove(struct i2c_client *client)
 
 	if (raw_data_buffer)
 		kfree(raw_data_buffer);
+
+	stmvl53l5_deinit(&client->dev);
+
 	return 0;
 }
 
@@ -337,6 +458,12 @@ static int stmvl53l5_spi_probe(struct spi_device *spi)
 
 	printk("stmvl53l5: probing spi\n");
 
+	ret = stmvl53l5_init(&spi->dev);
+	if (ret) {
+		pr_err("stmvl53l5: Error. Power up failed, ret = %d\n", ret);
+		return ret;
+	}
+
 	i2c_not_spi = 0;
 
 	spi_data.device = spi;
@@ -349,13 +476,16 @@ static int stmvl53l5_spi_probe(struct spi_device *spi)
 
 	if ((device_id != 0xF0) || (revision_id != 0x02)) {
 		pr_err("stmvl53l5: Error. Could not read device and revision id registers\n");
-		return ret;
+		ret = ret? ret : -ENXIO;
+		goto exit;
 	}
 	printk("stmvl53l5: device_id : 0x%x. revision_id : 0x%x\n", device_id, revision_id);
 
 	raw_data_buffer = kzalloc(VL53L5_COMMS_CHUNK_SIZE, GFP_DMA | GFP_KERNEL);
-	if (raw_data_buffer == NULL)
-		 return -ENOMEM;
+	if (raw_data_buffer == NULL) {
+		ret = -ENOMEM;
+		goto exit;
+	}
 
 	st_tof_miscdev.minor = MISC_DYNAMIC_MINOR;
 	st_tof_miscdev.name = "stmvl53l5";
@@ -365,13 +495,22 @@ static int stmvl53l5_spi_probe(struct spi_device *spi)
 	ret = misc_register(&st_tof_miscdev);
 	if (ret) {
 		pr_err("stmvl53l5 : Failed to create misc device, err = %d\n", ret);
-		return -1;
+		ret = -1;
+		goto exit;
 	}
 
 	misc_registered = 1;
 
 	return 0;
 
+exit:
+	if (raw_data_buffer) {
+		kfree(raw_data_buffer);
+		raw_data_buffer = NULL;
+	}
+	stmvl53l5_deinit(&spi->dev);
+
+	return ret;
 }
 
 static int stmvl53l5_spi_remove(struct spi_device *device)
@@ -379,6 +518,9 @@ static int stmvl53l5_spi_remove(struct spi_device *device)
 
 	if (raw_data_buffer)
 		kfree(raw_data_buffer);
+
+	stmvl53l5_deinit(&device->dev);
+
 	return 0;
 }
 
@@ -387,6 +529,7 @@ static struct spi_driver stmvl53l5_spi_driver = {
 	.driver = {
 		.name   = STMVL53L5_DRV_NAME,
 		.owner  = THIS_MODULE,
+		.of_match_table = of_match_ptr(st_tof_of_match), // for platform register to pick up the dts info
 	},
 	.probe  = stmvl53l5_spi_probe,
 	.remove = stmvl53l5_spi_remove,
