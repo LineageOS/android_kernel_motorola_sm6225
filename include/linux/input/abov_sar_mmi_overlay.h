5bd99f0807a35f99888e6494d540b4f12c0fabbd
chenlj2 <chenlj2@motorola.com>
2020-08-10 11:00:55 +0800

    DLKM:control VDD off when sar sensor is off
    It's used to save power.
    It's only used on Borneo NA
    
    Change-Id: I2aa60149f0a362dc29fc0eded5c509a9fd4ddc7c
    Signed-off-by: chenlj2 <chenlj2@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1718145
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Jiacheng Ou <oujc1@motorola.com>
    Reviewed-by: Guoyue Liu <liugy11@motorola.com>
    Reviewed-by: Wang Wang <wangwang1@lenovo.com>
    Submit-Approved: Jira Key

--- include/linux/input/abov_sar_mmi_overlay.h
+++ include/linux/input/abov_sar_mmi_overlay.h
@@ -232,10 +232,10 @@ static struct sensors_classdev sensors_capsensor_ch3_cdev = {
 
 
 typedef enum{
-	CAPSENSOR_ENABLE_FLAG_CH0,
-	CAPSENSOR_ENABLE_FLAG_CH1,
-	CAPSENSOR_ENABLE_FLAG_CH2,
-	CAPSENSOR_ENABLE_FLAG_CH3,
+	CAPSENSOR_ENABLE_FLAG_CH0=1,
+	CAPSENSOR_ENABLE_FLAG_CH1=2,
+	CAPSENSOR_ENABLE_FLAG_CH2=4,
+	CAPSENSOR_ENABLE_FLAG_CH3=8,
 	CAPSENSOR_ENABLE_FLAG_MAX
 }capsensor_enable_flag_t;
 /***************************************
