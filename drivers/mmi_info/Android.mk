3ddacf0cc466dddb90aaecbc83d130a695f4fe7c
xizx <xizx@motorola.com>
2020-08-15 16:31:07 +0800

    mmi_info: add dependency of mmi_annotate module
    mmi_info will depended on mmi_annotate module symbol,so it must
    define the dependeny in makefile.
    
    Here is the build error:
    ERROR: "mmi_annotate" [../../motorola/kernel/modules/drivers/mmi_info/mmi_info.ko] undefined!
    ERROR: "mmi_annotate_persist" [../../motorola/kernel/modules/drivers/mmi_info/mmi_info.ko] undefined!
    
    Change-Id: Ie3e929a1cffa543876c7a34447f8d659ecea0d9f
    Reviewed-on: https://gerrit.mot.com/1723102
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Guobin Zhang <zhanggb@motorola.com>
    Submit-Approved: Jira Key

--- drivers/mmi_info/Android.mk
+++ drivers/mmi_info/Android.mk
@@ -5,4 +5,5 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := mmi_info.ko
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
+LOCAL_ADDITIONAL_DEPENDENCIES := $(KERNEL_MODULES_OUT)/mmi_annotate.ko
 include $(DLKM_DIR)/AndroidKernelModule.mk
