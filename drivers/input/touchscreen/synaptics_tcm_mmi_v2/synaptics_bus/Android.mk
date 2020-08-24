610b9f510c2da677261d613c9933a05ed77abebd
litong <litong20@lenovo.com>
2020-08-24 14:27:59 +0800

    Specify the dependency of the synaptics touchscreen modules.
    (1) When all the modules are in a Kbuild file, there will be some errors
    that the exported symbols are undefined. Therefore the files with
    dependencies are split into independent Kbuilds, and the dependencies
    between the modules and the location of Module.symvers are clearly
    specified.
    (2) The synaptics_i2c and syanptics_spi modules have the same symbol
    table, so we should use BOARD_VENDOR_KERNEL_MODULES to determine
    which module to compile.
    
    Change-Id: I0e02d23562d0b52a20fdfd53d3db085cfffb811b
    Reviewed-on: https://gerrit.mot.com/1718101
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Jichao Zou <zoujc@motorola.com>
    Submit-Approved: Jira Key

---
+++ drivers/input/touchscreen/synaptics_tcm_mmi_v2/synaptics_bus/Android.mk
@@ -0,0 +1,20 @@
+DLKM_DIR := motorola/kernel/modules
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(DLKM_INSTALL_TO_VENDOR_OUT),true)
+SYNA_TCM_MODULE_PATH := $(TARGET_OUT_VENDOR)/lib/modules/
+else
+SYNA_TCM_MODULE_PATH := $(KERNEL_MODULES_OUT)
+endif
+
+include $(CLEAR_VARS)
+ifneq ($(findstring synaptics_i2c.ko,$(BOARD_VENDOR_KERNEL_MODULES)),)
+    KERNEL_CFLAGS += USE_TOUCHSCREEN_MMI_I2C=y
+    LOCAL_MODULE := synaptics_i2c.ko
+else
+    LOCAL_MODULE := synaptics_spi.ko
+endif
+    LOCAL_MODULE_TAGS := optional
+    LOCAL_MODULE_PATH := $(SYNA_TCM_MODULE_PATH)
+
+include $(DLKM_DIR)/AndroidKernelModule.mk
--- drivers/input/touchscreen/synaptics_tcm_mmi_v2/synaptics_extmodules/Android.mk
+++ drivers/input/touchscreen/synaptics_tcm_mmi_v2/synaptics_extmodules/Android.mk
@@ -4,6 +4,9 @@ LOCAL_PATH := $(call my-dir)
 ifneq ($(findstring touchscreen_mmi.ko,$(BOARD_VENDOR_KERNEL_MODULES)),)
 	KERNEL_CFLAGS += CONFIG_INPUT_TOUCHSCREEN_MMI=y
 endif
+ifeq ($(DRM_PANEL_NOTIFICATIONS),true)
+	KERNEL_CFLAGS += CONFIG_DRM_PANEL_NOTIFICATIONS=y
+endif
 
 ifeq ($(DLKM_INSTALL_TO_VENDOR_OUT),true)
 SYNA_TCM_MODULE_PATH := $(TARGET_OUT_VENDOR)/lib/modules/
@@ -12,58 +15,44 @@ SYNA_TCM_MODULE_PATH := $(KERNEL_MODULES_OUT)
 endif
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := synaptics_spi.ko
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_PATH := $(SYNA_TCM_MODULE_PATH)
-include $(DLKM_DIR)/AndroidKernelModule.mk
-
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := synaptics_i2c.ko
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_PATH := $(SYNA_TCM_MODULE_PATH)
-include $(DLKM_DIR)/AndroidKernelModule.mk
-
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := synaptics_core_module.ko
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_PATH := $(SYNA_TCM_MODULE_PATH)
-include $(DLKM_DIR)/AndroidKernelModule.mk
-
-include $(CLEAR_VARS)
 LOCAL_MODULE := synaptics_device.ko
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_PATH := $(SYNA_TCM_MODULE_PATH)
+LOCAL_ADDITIONAL_DEPENDENCIES := $(KERNEL_MODULES_OUT)/synaptics_core_module.ko
 include $(DLKM_DIR)/AndroidKernelModule.mk
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := synaptics_diagnostics.ko
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_PATH := $(SYNA_TCM_MODULE_PATH)
+LOCAL_ADDITIONAL_DEPENDENCIES := $(KERNEL_MODULES_OUT)/synaptics_core_module.ko
 include $(DLKM_DIR)/AndroidKernelModule.mk
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := synaptics_recovery.ko
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_PATH := $(SYNA_TCM_MODULE_PATH)
+LOCAL_ADDITIONAL_DEPENDENCIES := $(KERNEL_MODULES_OUT)/synaptics_core_module.ko
 include $(DLKM_DIR)/AndroidKernelModule.mk
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := synaptics_reflash.ko
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_PATH := $(SYNA_TCM_MODULE_PATH)
+LOCAL_ADDITIONAL_DEPENDENCIES := $(KERNEL_MODULES_OUT)/synaptics_core_module.ko
 include $(DLKM_DIR)/AndroidKernelModule.mk
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := synaptics_testing.ko
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_PATH := $(SYNA_TCM_MODULE_PATH)
+LOCAL_ADDITIONAL_DEPENDENCIES := $(KERNEL_MODULES_OUT)/synaptics_core_module.ko
 include $(DLKM_DIR)/AndroidKernelModule.mk
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := synaptics_zeroflash.ko
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_PATH := $(SYNA_TCM_MODULE_PATH)
+LOCAL_ADDITIONAL_DEPENDENCIES := $(KERNEL_MODULES_OUT)/synaptics_core_module.ko
 include $(DLKM_DIR)/AndroidKernelModule.mk
 
