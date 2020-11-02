58d20fda4866661e7d7704471039225f44673b9c
weimh1 <weimh1@lenovo.com>
2020-11-02 11:12:00 +0800

    driver/sar-sensor: build error on user build
      build error:
        ERROR: "sensors_classdev_unregister" [../../motorola/kernel/modules/
        drivers/misc/sx937x/sx937x_sar.ko] undefined!
        ERROR: "sensors_classdev_register" [../../motorola/kernel/modules/
        drivers/misc/sx937x/sx937x_sar.ko] undefined!
    
      These two symbols defined on sensors_class.ko.But it will create gki 
      for user build,so we should fix it as below:
      KBUILD_EXTRA_SYMBOLS += $(CURDIR)/$(KBUILD_EXTMOD)/
      ../../sensors/gki/Module.symvers
    
      MODPOST = scripts/mod/modpost					\
            $(if $(CONFIG_MODVERSIONS),-m)				\
    	$(if $(CONFIG_MODULE_SRCVERSION_ALL),-a)		\
    	$(if $(KBUILD_EXTMOD),-i,-o) $(kernelsymfile)		\
    	$(if $(KBUILD_EXTMOD),-I $(modulesymfile))		\
    	$(if $(KBUILD_EXTMOD),$(addprefix -e ,$(KBUILD_EXTRA_SYMBOLS)))\
    	$(if $(KBUILD_EXTMOD),-o $(modulesymfile))		\
    	$(if $(CONFIG_SECTION_MISMATCH_WARN_ONLY),,-E)		\
    	$(if $(KBUILD_MODPOST_WARN),-w)				\
    	$(if $(filter nsdeps,$(MAKECMDGOALS)),-d)
    
    Change-Id: Ib402dcca29637ec77d805200cc768b1b2cd2d961
    Signed-off-by: weimh1 <weimh1@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/1790594
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Submit-Approved: Jira Key

--- drivers/misc/sx937x/Android.mk
+++ drivers/misc/sx937x/Android.mk
@@ -5,6 +5,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := sx937x_sar.ko
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
+KBUILD_OPTIONS_GKI += GKI_OBJ_MODULE_DIR=gki
 include $(DLKM_DIR)/AndroidKernelModule.mk
 
 ifeq ($(SX937X_USB_CAL),true)
