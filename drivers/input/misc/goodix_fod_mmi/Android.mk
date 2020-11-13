ba80144174a7fc6c609be99e91487d6fb2510dc4
Tito Ricardo Bianchin Oliveira <tito2@motorola.com>
2020-11-13 12:13:18 -0600

    Revert "(CR): Register mmi_relay notification for FOD"
    Cause: This commit makes Racer5G touch doesn't work at all
    
    This reverts commit 488e073dc33db9ba86fdfb47d1e010d7fa65f9fb.
    
    Change-Id: I3c4f25b2290cd39267ecee3a1061ee5775f2a844
    Reviewed-on: https://gerrit.mot.com/1802576
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Reviewed-by: Sergio Vilela <sv008@motorola.com>
    Submit-Approved: Sergio Vilela <sv008@motorola.com>
    Tested-by: Sergio Vilela <sv008@motorola.com>

--- drivers/input/misc/goodix_fod_mmi/Android.mk
+++ drivers/input/misc/goodix_fod_mmi/Android.mk
@@ -5,7 +5,5 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := goodix_fod_mmi.ko
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
-LOCAL_ADDITIONAL_DEPENDENCIES := $(KERNEL_MODULES_OUT)/mmi_relay.ko
-KBUILD_OPTIONS_GKI += GKI_OBJ_MODULE_DIR=gki
 include $(DLKM_DIR)/AndroidKernelModule.mk
 
