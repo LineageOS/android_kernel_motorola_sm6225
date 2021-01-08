e1e7ada35946f420d64b241a64656a2294be1e36
xuwt2 <xuwt2@lenovo.com>
2021-01-08 17:32:27 +0800

    qpnp-smbcharger-mmi: Add PD charGing in DLKM
    PD3.0 with PPS is a Mandatory requirement from VZW,
    Add PD charGing(include PPS/Fixed) policy in DLKM for single
    charge path(only pmic charGing)
    
    Change-Id: Ic0f2fcf45f00dea214602846bdbe43b56805a114
    Signed-off-by: xuwt2 <xuwt2@mt.com>
    Reviewed-on: https://gerrit.mot.com/1847093
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Haijian Ma <mahj8@motorola.com>
    Reviewed-by: Yonghua Yan <yanyh2@motorola.com>
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Submit-Approved: Jira Key

--- drivers/power/qpnp-smbcharger-mmi/Android.mk
+++ drivers/power/qpnp-smbcharger-mmi/Android.mk
@@ -1,6 +1,10 @@
 DLKM_DIR := motorola/kernel/modules
 LOCAL_PATH := $(call my-dir)
 
+ifeq ($(MOTO_PD_CHARGER),true)
+	KERNEL_CFLAGS += CONFIG_PD_CHARGER_MMI=y
+endif
+
 include $(CLEAR_VARS)
 LOCAL_MODULE := qpnp-smbcharger-mmi.ko
 LOCAL_MODULE_TAGS := optional
