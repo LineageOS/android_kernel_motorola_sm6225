1e53c573f9acfa7f0b2f27b776519536d76f16dc
xuwei9 <xuwei9@lenovo.com>
2021-02-09 15:29:34 +0800

    kernel:charger rate duplicate with parallel charger
    Only define charger rate without
    paralle charger not define
    
    Change-Id: Ib25cf50ccf6f6e3cfa30fa60bad37df7552c6b18
    Signed-off-by: xuwei9 <xuwei9@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/1881083
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key

--- drivers/power/qpnp-smbcharger-mmi/Android.mk
+++ drivers/power/qpnp-smbcharger-mmi/Android.mk
@@ -5,6 +5,10 @@ ifeq ($(MOTO_PD_CHARGER),true)
 	KERNEL_CFLAGS += CONFIG_PD_CHARGER_MMI=y
 endif
 
+ifeq ($(BQ2597X_DUAL_ENABLE),true)
+        KERNEL_CFLAGS += CONFIG_BQ2597X_DUAL=y
+endif
+
 include $(CLEAR_VARS)
 LOCAL_MODULE := qpnp-smbcharger-mmi.ko
 LOCAL_MODULE_TAGS := optional
