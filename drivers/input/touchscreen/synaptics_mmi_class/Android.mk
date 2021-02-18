ffce9d9937a0484eece9f8896f83b8b3c921182e
Konstantin Makariev <kmakariev@motorola.com>
2021-02-18 12:19:24 -0600

    Revert "(CR) synaptics_mmi_class: banks cli bringup"
    This reverts commit e27b5c3a28177c7b9756b3b62aca8378252557a3.
    
    Mot-CRs-fixed: (CR)
    
    Change-Id: I11010cf049ea29c0967df11a79a7410cd99c81d5
    Reviewed-on: https://gerrit.mot.com/1881606
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key

--- drivers/input/touchscreen/synaptics_mmi_class/Android.mk
+++ drivers/input/touchscreen/synaptics_mmi_class/Android.mk
@@ -8,8 +8,6 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := synaptics_mmi.ko
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
-LOCAL_ADDITIONAL_DEPENDENCIES += $(KERNEL_MODULES_OUT)/mmi_info.ko
-LOCAL_ADDITIONAL_DEPENDENCIES += $(KERNEL_MODULES_OUT)/touchscreen_mmi.ko
 include $(DLKM_DIR)/AndroidKernelModule.mk
 
 include $(CLEAR_VARS)
@@ -30,5 +28,4 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
 include $(DLKM_DIR)/AndroidKernelModule.mk
 
-KBUILD_OPTIONS_GKI += GKI_OBJ_MODULE_DIR=gki
 endif
