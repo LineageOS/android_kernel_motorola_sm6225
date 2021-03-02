2ed9f712d05bd4b2837d89e2ad5f74dae64a9de9
Konstantin Makariev <hcv867@motorola.com>
2021-03-01 19:11:17 -0600

    bos0614_mmi: added features
    Added the following features:
    
      1) BOS0614 registers dump sysfs
      2) reading device tree and applying default config
         in case of power cut
    
    Change-Id: I53f1b7069ea4d74aa8b02ac525765951fe29bcce
    Signed-off-by: Konstantin Makariev <hcv867@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1889879
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Ling Jin <lingjin@motorola.com>
    Submit-Approved: Jira Key

--- drivers/input/misc/bos0614_mmi/i2cLinux.c
+++ drivers/input/misc/bos0614_mmi/i2cLinux.c
@@ -21,12 +21,24 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 //
+#define pr_fmt(fmt) "bos0614: %s: " fmt, __func__
+
+#define DEBUG
+//#undef DEBUG
+
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/slab.h>
 #include <linux/types.h>
 
 #include "i2cLinux.h"
+
+#ifdef DEBUG
+#undef pr_debug
+#define pr_debug pr_err
+#undef dev_dbg
+#define dev_dbg dev_err
+#endif
 
 typedef struct
 {
@@ -39,6 +51,124 @@ static void initDriverFct(Context *ctx);
 #define NBR_CHAR_PER_BYTE (5)
 #define ESCAPE_CHAR (1)
 
+int process_params(ParamsLst *params, int numP, const char *buffer)
+{
+	char *arg, *buf, *p;
+	int n, err;
+	unsigned int value;
+
+	p = buf = kstrdup(buffer, GFP_KERNEL);
+	for (n = 0; n < numP && p && *p; n++, params++) {
+		arg = strsep(&p, " ");
+		if (!arg || !*arg)
+			break;
+
+		pr_debug("arg=[%s] rest=[%s]\n", arg, p);
+
+		switch (params->tid) {
+		case PARAM_UCHAR8:
+			err = kstrtou8(arg, 0, params->data_ptr);
+			value = (unsigned int)*(unsigned char *)params->data_ptr;
+				break;
+		case PARAM_INT16:
+			err = kstrtos16(arg, 0, params->data_ptr);
+			value = (unsigned int)*(short *)params->data_ptr;
+				break;
+		case PARAM_UINT16:
+			err = kstrtou16(arg, 0, params->data_ptr);
+			value = (unsigned int)*(unsigned short *)params->data_ptr;
+				break;
+		case PARAM_INT32:
+			err = kstrtoint(arg, 0, params->data_ptr);
+			value = (unsigned int)*(int *)params->data_ptr;
+				break;
+		case PARAM_UINT32:
+			err = kstrtouint(arg, 0, params->data_ptr);
+			value = *(unsigned int *)params->data_ptr;
+				break;
+		}
+
+		if (err) {
+			n = err;
+			break;
+		} else
+			pr_debug("[%d]=%u\n", n, value);
+	}
+	kfree(buf);
+	pr_debug("processed %d input parameters\n", n);
+	return n;
+}
+#if 0
+static int readRegsConfig(Context *ctx, struct device_node *parent, const char *suffix)
+{
+	Bos0614RegisterStruct *config = getAllRegsPtr();
+	struct device_node *node;
+	char node_name[64];
+	u32 *temp, length = 0;
+	int npairs, i, index, ret = -EIO;
+
+	scnprintf(node_name, 63, "config-%s", suffix);
+	node = of_find_node_by_name(parent, node_name);
+	if (!node)
+		return -ENODEV;
+
+	if (!of_find_property(node, "config-data", &length)) {
+		dev_err(&ctx->client->dev, "(config-%s) prop config-data not found\n", suffix);
+		goto out;
+	}
+
+	npairs = length / 2;
+	dev_info(&ctx->client->dev, "(config-%s) array size %d\n", suffix, npairs);
+
+	temp = kzalloc(length, GFP_KERNEL);
+	if (!temp)
+		goto out;
+
+	ret = of_property_read_u32_array(node, "config-data", temp, sizeof(u32) * npairs * 2);
+	if (ret) {
+		dev_err(&ctx->client->dev, "error reading config-data (config-%s)\n", suffix);
+		goto release_mem;
+	}
+
+	for (i = 0; i < npairs; i++) {
+		index =(uint16_t)*temp++;
+		config[index].value = (uint16_t)*temp++;
+		dev_info(&ctx->client->dev, "[%d] addr=0x%02x, val=0x%04x\n",
+				i, config[index].addr, config[index].value);
+	}
+
+release_mem:
+	kfree(temp);
+out:
+	of_node_put(node);
+
+	return ret;
+}
+
+static int readDevTree(Context *ctx)
+{
+	int ret, verno;
+	struct device_node *np = ctx->client->dev.of_node;
+	struct device_node *config_np;
+
+	config_np = of_find_node_by_name(np, "configs");
+	if (!config_np) {
+		dev_info(&ctx->client->dev, "does not support configs\n");
+		return 0;
+	}
+
+	if (!of_property_read_u32(config_np, "config-ver", &verno))
+		dev_info(&ctx->client->dev, "dt config Rev.%u\n", verno);
+
+	ret = readRegsConfig(ctx, config_np, "default");
+	if (ret > 0)
+		dev_info(&ctx->client->dev, "has default config\n");
+
+	of_node_put(config_np);
+
+	return 0;
+}
+#endif
 static void logBuffer(const char *message, const void *data, size_t length)
 {
     size_t bufferLength = length * NBR_CHAR_PER_BYTE + ESCAPE_CHAR;
@@ -57,14 +187,12 @@ static void logBuffer(const char *message, const void *data, size_t length)
             ptr += sprintf(ptr, index < (length - 1) ? "0x%02x " : "0x%02x", _data[index]);
         }
 
-        //printk("%s length: %zu [%s]\n", message, length, buf, bufferLength, ptr, end);
-        printk("%s length: %zu [%s]\n", message, length, buf);
+        pr_debug("%s length: %zu [%s]\n", message, length, buf);
 
         kfree(buf);
     }
 
 }
-
 
 /*
  * Private Section
@@ -142,15 +270,19 @@ I2c *i2cBoreasLinuxInit(struct i2c_client *client)
     {
         Context *ctx = kzalloc(sizeof(Context), GFP_KERNEL);
 
-        printk("[PF-Debug] I2C Client %p", client);
-
         if (ctx != NULL)
         {
             ctx->client = client;
             initDriverFct(ctx);
             driver = &ctx->driver;
+#if 0
+            if (ctx->client->dev.of_node)
+               readDevTree(ctx);
+#endif
         }
     }
+
+    pr_debug("I2C client: %p, driver: %p\n", client, driver);
 
     return driver;
 }
