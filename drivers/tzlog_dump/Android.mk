1f07e768c64a4807ac4592d633340c48408dc041
a17671 <a17671@motorola.com>
2021-06-09 22:31:32 +0800

    Add watchdog_cpu_dump and tzlog_dump support on 5.4
    Add watchdog_cpu context and tzlog dump support
    For 5.4 or later kernel, there is no way to browse the
    Symbols without breaking the GKI rules
    So read the ttbl1_el1 to get the init_mm->pgd instead of
    The old symbols browsing
    Update module dependencies
    
    Change-Id: I26aca73f4a879f9504ecd5443c458e154cf48fe9
    Signed-off-by: a17671 <a17671@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1975101
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key

--- drivers/tzlog_dump/Android.mk
+++ drivers/tzlog_dump/Android.mk
@@ -4,6 +4,10 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := tzlog_dump.ko
 LOCAL_MODULE_TAGS := optional
+ifeq ($(DLKM_INSTALL_TO_VENDOR_OUT),true)
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR)/lib/modules/
+else
 LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
+endif
 LOCAL_ADDITIONAL_DEPENDENCIES := $(KERNEL_MODULES_OUT)/mmi_info.ko
 include $(DLKM_DIR)/AndroidKernelModule.mk
