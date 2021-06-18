e7b1662f5cea6ccf362e598504b3dab8c6bb3a8d
xuwt2 <xuwt2@lenovo.com>
2021-06-18 12:04:34 +0800

    qpnp-adaptive-charge: use mmi_charger to contrl battery charging
    In QCOM 7350/8350, the kernel doesn't provide pmic-vote to control
    battery charging.
    the mmi_charger is an intermediate layer that forwards charging control
    to ADSP Battmngr from the application layer.
    By using USE_MMI_CHARGER macro definition that qpnp-adaptive-charge can be use
    mmi_charger's func to control battery charging
    
    Change-Id: I4da0604eabdadfc483143360ef7b7aad174cda47
    Signed-off-by: xuwt2 <xuwt2@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/1983612
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Yonghua Yan <yanyh2@motorola.com>
    Reviewed-by: Jianqi Yang <yangj@motorola.com>
    Submit-Approved: Jira Key

--- drivers/power/qpnp_adaptive_charge/Android.mk
+++ drivers/power/qpnp_adaptive_charge/Android.mk
@@ -4,6 +4,12 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := qpnp_adaptive_charge.ko
 LOCAL_MODULE_TAGS := optional
+
+ifeq ($(USE_MMI_CHARGER), true)
+		KERNEL_CFLAGS += CONFIG_USE_MMI_CHARGER=y
+LOCAL_ADDITIONAL_DEPENDENCIES := $(KERNEL_MODULES_OUT)/mmi_charger.ko
+endif
+
 LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
 include $(DLKM_DIR)/AndroidKernelModule.mk
 
