72d85af514874679efd5043b7cd8586ea03ab2ce
cuijy1 <cuijy2@lenovo.com>
2021-08-03 10:11:58 +0800

    hall driver bring up
    add hall pen detect ko
    add hall folio detect ko
    
    Change-Id: Idd325b51487f3d9313c56034adadf4169094f0e6
    Signed-off-by: cuijy1 <cuijy2@mt.com>
    Reviewed-on: https://gerrit.mot.com/2025362
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Ningkai Zhuang <zhuangnk1@mt.com>
    Reviewed-by: Tao Sun <suntao2@motorola.com>
    Submit-Approved: Jira Key

---
+++ drivers/misc/hall/Android.mk
@@ -0,0 +1,16 @@
+DLKM_DIR := motorola/kernel/modules
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := hall_pen.ko
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
+KBUILD_OPTIONS_GKI += GKI_OBJ_MODULE_DIR=gki
+include $(DLKM_DIR)/AndroidKernelModule.mk
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := hall_folio.ko
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
+KBUILD_OPTIONS_GKI += GKI_OBJ_MODULE_DIR=gki
+include $(DLKM_DIR)/AndroidKernelModule.mk
