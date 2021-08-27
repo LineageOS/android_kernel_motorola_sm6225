ac31279ac7f49df11176c88cdffaea933dabc614
Jichao Zou <zoujc@motorola.com>
2021-08-27 17:42:01 +0800

    mmi_sys_temp:fix compilation error.
    motorola/kernel/modules/drivers/misc/mmi_sys_temp/mmi_sys_temp.c:118:23: error: implicit declaration of function 'compat_ptr' [-Werror,-Wimplicit-function-declaration]
            arg = (unsigned long)compat_ptr(arg);
    
    Change-Id: Ifba590fd36a743e7d62df81c82fcc4839b7c816a
    Signed-off-by: Jichao Zou <zoujc@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2053149
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Jianqi Yang <yangj@motorola.com>
    Submit-Approved: Jira Key

--- drivers/misc/mmi_sys_temp/Android.mk
+++ drivers/misc/mmi_sys_temp/Android.mk
@@ -4,10 +4,6 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := mmi_sys_temp.ko
 LOCAL_MODULE_TAGS := optional
-ifeq ($(DLKM_INSTALL_TO_VENDOR_OUT),true)
-LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR)/lib/modules/
-else
 LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
-endif
-
+KBUILD_OPTIONS_GKI += GKI_OBJ_MODULE_DIR=gki
 include $(DLKM_DIR)/AndroidKernelModule.mk
