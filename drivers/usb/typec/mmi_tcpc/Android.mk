2065b62ede136c971a3ccee671c83c8127176036
chailu1 <chailu1@lenovo.com>
2021-08-31 13:56:57 +0800

    typec: Add sgm7220 driver initial version
    typec/sgm7220
    Add sgm7220 driver initial version
    
    Change-Id: I5b9582d369a574b16c5cf13a5016c0d51ddc9abf
    Signed-off-by: chailu1 <chailu1@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/2041140
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Submit-Approved: Jira Key

--- drivers/usb/typec/mmi_tcpc/Android.mk
+++ drivers/usb/typec/mmi_tcpc/Android.mk
@@ -1,6 +1,10 @@
 DLKM_DIR := motorola/kernel/modules
 LOCAL_PATH := $(call my-dir)
 
+#ifeq ($(TCPC_SGM7220),true)
+	KERNEL_CFLAGS += CONFIG_TCPC_SGM7220=y
+#endi
+
 #ifeq ($(TCPC_RT1711H),true)
 	KERNEL_CFLAGS += CONFIG_TCPC_RT1711H=y
 #endif
@@ -12,6 +16,12 @@ LOCAL_PATH := $(call my-dir)
 #ifeq ($(USB_POWER_DELIVERY),true)
 	KERNEL_CFLAGS += CONFIG_USB_POWER_DELIVERY=y
 #endif
+
+include $(CLEAR_VARS)
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE := tcpc_sgm7220.ko
+LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
+include $(DLKM_DIR)/AndroidKernelModule.mk
 
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
