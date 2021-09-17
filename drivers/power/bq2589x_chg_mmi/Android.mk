f752b45fe5fb1d97d46a6086106e5c0c9b4ad595
gongsc1 <gongsc1@motorola.com>
2021-09-17 11:19:57 +0800

    power/charger: add init bq25890 driver
    add init bq25890 driver from github
    https://github.com/tibms/kernel-4.4/tree/release/drivers/power/bq2589x-master
    for milanf
    
    Change-Id: Icb7b25364a4af1e01644286dbad2f0dda5038840
    Reviewed-on: https://gerrit.mot.com/2069663
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Submit-Approved: Jira Key

---
+++ drivers/power/bq2589x_chg_mmi/Android.mk
@@ -0,0 +1,13 @@
+DLKM_DIR := motorola/kernel/modules
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE := bq2589x_charger.ko
+
+
+LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
+
+
+
+include $(DLKM_DIR)/AndroidKernelModule.mk
