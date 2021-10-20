d3fb20bfaf6b2ac483dcb3af563d7dfe6cb3a256
mahj8 <mahj8@lenovo.com>
2021-10-20 10:17:52 +0800

    charGing: add iio psy to provide api for other modules
    add iio psy frame to provide api for
    other modules
    
    Change-Id: Ibc8478b4ee9b07b843f262555e1d15c5a59fc554
    Signed-off-by: mahj8 <mahj8@mt.com>
    Reviewed-on: https://gerrit.mot.com/2088633
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Submit-Approved: Jira Key

---
+++ drivers/power/mmi_discrete_charger/mmi_discrete_charger_iio.h
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2020 Motorola Mobility LLC
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef __MMI_DISCRETE_CHARGER_IIO_H__
+#define __MMI_DISCRETE_CHARGER_IIO_H__
+
+#include <linux/platform_device.h>
+#include <linux/iio/consumer.h>
+#include <linux/iio/iio.h>
+#include <dt-bindings/iio/qti_power_supply_iio.h>
+#include "mmi_discrete_charger_core.h"
+
+struct mmi_discrete_iio_channels {
+	const char *datasheet_name;
+	int channel_num;
+	enum iio_chan_type type;
+	long info_mask;
+};
+
+#define MMI_DISCRETE_IIO_CHAN(_name, _num, _type, _mask)		\
+	{						\
+		.datasheet_name = _name,		\
+		.channel_num = _num,			\
+		.type = _type,				\
+		.info_mask = _mask,			\
+	},
+
+#define MMI_DISCRETE_CHAN_INDEX(_name, _num)			\
+	MMI_DISCRETE_IIO_CHAN(_name, _num, IIO_INDEX,		\
+		BIT(IIO_CHAN_INFO_PROCESSED))
+
+static const struct mmi_discrete_iio_channels mmi_discrete_iio_psy_channels[] = {
+	MMI_DISCRETE_CHAN_INDEX("usb_real_type", PSY_IIO_USB_REAL_TYPE)
+	MMI_DISCRETE_CHAN_INDEX("otg_enable", PSY_IIO_MMI_OTG_ENABLE)
+};
+
+int mmi_discrete_init_iio_psy(struct mmi_discrete_charger *chip,
+				struct platform_device *pdev);
+
+#endif
