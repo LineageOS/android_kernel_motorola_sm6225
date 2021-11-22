400a7f952b27bdd628a026a7364fd2ee8513e5c8
chenzc2 <chenzc2@lenovo.com>
2021-11-22 16:45:25 +0800

    Rhode4G: Implement different ldo compatibility.
    Below it is the compatible ldo information.
    a.wl2868c.
    b.et5907.
    c.fan53870.
    
    Change-Id: I6bf57bc4420eded96410568323a720c2caafa8e2
    Signed-off-by: chenzc2 <chenzc2@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/2123413
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: PengFei Qiao <qiaopf1@motorola.com>
    Reviewed-by: Zhuoran Xu <xuzr3@motorola.com>
    Reviewed-by: Jian Luan <jamesluan@motorola.com>
    Reviewed-by: Long Cheng <chengl1@motorola.com>
    Reviewed-by: Guobin Zhang <zhanggb@motorola.com>
    Submit-Approved: Jira Key

--- drivers/regulator/wl2868c/wl2868c-regulator.h
+++ drivers/regulator/wl2868c/wl2868c-regulator.h
@@ -6,42 +6,57 @@
  * Author: ChengLong, Motorola Mobility LLC,
  */
 
-#ifndef __wl2868c_REGISTERS_H__
-#define __wl2868c_REGISTERS_H__
+#ifndef __WL2868C_REGISTERS_H__
+#define __WL2868C_REGISTERS_H__
 
 /* Registers */
-#define wl2868c_REG_NUM (wl2868c_SEQ_STATUS - wl2868c_CHIP_REV + 1)
+#define WL2868C_REG_NUM (WL2868C_SEQ_STATUS - WL2868C_CHIP_REV + 1)
 
-#define wl2868c_CHIP_REV 0x00
-#define wl2868c_CURRENT_LIMITSEL 0x01
-#define wl2868c_DISCHARGE_RESISTORS 0x02
-#define wl2868c_LDO1_VOUT 0x03
-#define wl2868c_LDO2_VOUT 0x04
-#define wl2868c_LDO3_VOUT 0x05
-#define wl2868c_LDO4_VOUT 0x06
-#define wl2868c_LDO5_VOUT 0x07
-#define wl2868c_LDO6_VOUT 0x08
-#define wl2868c_LDO7_VOUT 0x09
-#define wl2868c_LDO1_LDO2_SEQ 0x0a
-#define wl2868c_LDO3_LDO4_SEQ 0x0b
-#define wl2868c_LDO5_LDO6_SEQ 0x0c
-#define wl2868c_LDO7_SEQ 0x0d
-#define wl2868c_LDO_EN 0x0e
-#define wl2868c_SEQ_STATUS 0x0f
+#define WL2868C_CHIP_REV            0x01
+#define WL2868C_CURRENT_LIMITSEL    0x01
+#define WL2868C_DISCHARGE_RESISTORS 0x02
+
+#define WL2868C_LDO1_VOUT   0x03
+#define WL2868C_LDO2_VOUT   0x04
+#define WL2868C_LDO3_VOUT   0x05
+#define WL2868C_LDO4_VOUT   0x06
+#define WL2868C_LDO5_VOUT   0x07
+#define WL2868C_LDO6_VOUT   0x08
+#define WL2868C_LDO7_VOUT   0x09
+
+#define WL2868C_LDO1_LDO2_SEQ   0x0a
+#define WL2868C_LDO3_LDO4_SEQ   0x0b
+#define WL2868C_LDO5_LDO6_SEQ   0x0c
+#define WL2868C_LDO7_SEQ        0x0d
+#define WL2868C_LDO_EN          0x0e
+
+#define WL2868C_SEQ_STATUS      0x0f
 
 
-/* wl2868c_LDO1_VSEL ~ wl2868c_LDO7_VSEL =
+#define fan53870_LDO1_VOUT   0x04
+#define fan53870_LDO2_VOUT   0x05
+#define fan53870_LDO3_VOUT   0x06
+#define fan53870_LDO4_VOUT   0x07
+#define fan53870_LDO5_VOUT   0x08
+#define fan53870_LDO6_VOUT   0x09
+#define fan53870_LDO7_VOUT   0x0a
+
+#define fan53870_LDO_EN        0x03
+#define fan53870_LDO_OFFSET    0x01
+#define fan53870_ET5907_ADDR   0x35
+
+/* WL2868C_LDO1_VSEL ~ WL2868C_LDO7_VSEL =
  * 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09
  */
-#define  wl2868c_LDO1_VSEL                      wl2868c_LDO1_VOUT
-#define  wl2868c_LDO2_VSEL                      wl2868c_LDO2_VOUT
-#define  wl2868c_LDO3_VSEL                      wl2868c_LDO3_VOUT
-#define  wl2868c_LDO4_VSEL                      wl2868c_LDO4_VOUT
-#define  wl2868c_LDO5_VSEL                      wl2868c_LDO5_VOUT
-#define  wl2868c_LDO6_VSEL                      wl2868c_LDO6_VOUT
-#define  wl2868c_LDO7_VSEL                      wl2868c_LDO7_VOUT
+#define  WL2868C_LDO1_VSEL                      WL2868C_LDO1_VOUT
+#define  WL2868C_LDO2_VSEL                      WL2868C_LDO2_VOUT
+#define  WL2868C_LDO3_VSEL                      WL2868C_LDO3_VOUT
+#define  WL2868C_LDO4_VSEL                      WL2868C_LDO4_VOUT
+#define  WL2868C_LDO5_VSEL                      WL2868C_LDO5_VOUT
+#define  WL2868C_LDO6_VSEL                      WL2868C_LDO6_VOUT
+#define  WL2868C_LDO7_VSEL                      WL2868C_LDO7_VOUT
 
-#define  wl2868c_VSEL_SHIFT                     0
-#define  wl2868c_VSEL_MASK                      (0xff << 0)
+#define  WL2868C_VSEL_SHIFT                     0
+#define  WL2868C_VSEL_MASK                      (0xff << 0)
 
-#endif /* __wl2868c_REGISTERS_H__ */
+#endif /* __WL2868C_REGISTERS_H__ */
