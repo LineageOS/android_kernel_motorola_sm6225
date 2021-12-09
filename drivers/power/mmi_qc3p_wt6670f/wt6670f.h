4ac109835c8baec474575ef494eb0b115ee18683
xuwei9 <xuwei9@lenovo.com>
2021-12-09 11:23:47 +0800

    QC3P:add iio sys support
    Add iio sys support for
    QC3P turbo charger.
    
    Change-Id: Ib8c626e912becd75d751fda7e6050a39fcc0785c
    Signed-off-by: xuwei9 <xuwei9@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/2139278
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Submit-Approved: Jira Key

--- drivers/power/mmi_qc3p_wt6670f/wt6670f.h
+++ drivers/power/mmi_qc3p_wt6670f/wt6670f.h
@@ -1,5 +1,8 @@
 #ifndef  __QC3P_WT6670_
 #define __QC3P_WT6670_
+#include <linux/iio/iio.h>
+#include <dt-bindings/iio/qti_power_supply_iio.h>
+#include <linux/iio/consumer.h>
 
 #define WT6670_FIRMWARE_VERSION  0x03
 
@@ -14,6 +17,9 @@ struct wt6670f {
 	int count;
 	int chg_type;
 	bool chg_ready;
+	struct iio_dev		*indio_dev;
+	struct iio_chan_spec	*iio_chan;
+	struct iio_channel	*int_iio_chans;
 
 	struct mutex i2c_rw_lock;
 };
@@ -21,6 +27,47 @@ struct wt6670f {
 enum {
 	QC3P_WT6670F,
 	QC3P_Z350,
+};
+
+enum wt6670_charger_type{
+	WT6670_CHG_TYPE_BEGIN = 0,
+	WT6670_CHG_TYPE_FC,
+	WT6670_CHG_TYPE_SDP,
+	WT6670_CHG_TYPE_CDP,
+	WT6670_CHG_TYPE_DCP,
+	WT6670_CHG_TYPE_QC2,
+	WT6670_CHG_TYPE_QC3,
+	WT6670_CHG_TYPE_OCP,
+	WT6670_CHG_TYPE_QC3P_18W,
+	WT6670_CHG_TYPE_QC3P_27W,
+	WT6670_CHG_TYPE_UNKNOWN,
+};
+
+struct wt6670_iio_channels {
+	const char *datasheet_name;
+	int channel_num;
+	enum iio_chan_type type;
+	long info_mask;
+};
+
+#define wt6670_IIO_CHAN(_name, _num, _type, _mask)		\
+	{						\
+		.datasheet_name = _name,		\
+		.channel_num = _num,			\
+		.type = _type,				\
+		.info_mask = _mask,			\
+	},
+
+#define wt6670_CHAN_INDEX(_name, _num)			\
+	wt6670_IIO_CHAN(_name, _num, IIO_INDEX,		\
+		BIT(IIO_CHAN_INFO_PROCESSED))
+
+static const struct wt6670_iio_channels wt6670_iio_psy_channels[] = {
+	wt6670_CHAN_INDEX("wt6670_usb_real_type", PSY_IIO_QC3P_REAL_TYPE)
+	wt6670_CHAN_INDEX("wt6670_usb_qc3p_power", PSY_IIO_QC3P_POWER)
+	wt6670_CHAN_INDEX("wt6670_battery_dp_dm", PSY_IIO_BATTERY_DP_DM)
+	wt6670_CHAN_INDEX("wt6670_start_detection", PSY_IIO_START_DETECTION)
+	wt6670_CHAN_INDEX("wt6670_detection_ready", PSY_IIO_DETECTION_READY)
 };
 
 int wt6670f_isp_flow(struct wt6670f *chip);
