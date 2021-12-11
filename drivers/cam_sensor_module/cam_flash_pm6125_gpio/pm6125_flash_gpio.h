8f2eadbf24d0a6d634023fc555b1827cdc0919e1
huaqin78 <huaqin78@motorola.com>
2021-12-11 18:14:05 +0800

    Rhode: flash node control
    details:
    control flash enable & brightness
    
    Change-Id: I21826056b5c31aaaff6e745477be333167eaa382
    Signed-off-by: huaqin78 <huaqin78@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2142109
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: <huaqin90@motorola.com>
    Reviewed-by: Long Cheng <chengl1@motorola.com>
    Reviewed-by: Xingyun Fu <fuxy7@motorola.com>
    Reviewed-by: Wang Liu <liuwang2@mt.com>
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    Submit-Approved: Jira Key

--- drivers/cam_sensor_module/cam_flash_pm6125_gpio/pm6125_flash_gpio.h
+++ drivers/cam_sensor_module/cam_flash_pm6125_gpio/pm6125_flash_gpio.h
@@ -12,6 +12,14 @@ typedef enum {
 	PM6125_FLASH_GPIO_STATE_MAX,	/* for array size */
 } PM6125_FLASH_GPIO_STATE;
 
+#ifdef CONFIG_CAMERA_FLASH_PWM
+enum{
+	CAMERA_SENSOR_FLASH_STATUS_OFF,
+	CAMERA_SENSOR_FLASH_STATUS_LOW,
+	CAMERA_SENSOR_FLASH_STATUS_HIGH
+};
+#endif
+
 #define PM6125_PWM_PERIOD 50000
 #define FLASH_FIRE_HIGH_MAXCURRENT 1200
 #define FLASH_FIRE_LOW_MAXCURRENT 150
@@ -20,4 +28,9 @@ typedef enum {
  * Function Prototype
  *****************************************************************************/
 extern void pm6125_flash_gpio_select_state(PM6125_FLASH_GPIO_STATE s, enum camera_flash_opcode opcode, u64 flash_current);
+
+#ifdef CONFIG_CAMERA_FLASH_PWM
+extern int pm6125_flash_control_create_device(struct device* dev);
+extern int pm6125_flash_control_remove_device(struct device* dev);
+#endif
 #endif /* _PM6125_FLASH_GPIO_H_*/
