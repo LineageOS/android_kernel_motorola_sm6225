bab07d0d967213351c0c643c8d0f080d7d686e83
huaqin87 <huaqin87@motorola.com>
2021-12-13 12:05:28 +0800

    wt6670: wake up wt6670 when get version num
    pull up and pull down intb pin when read version num
    and update new firmware
    
    Change-Id: I3e2cd65e96993e26206ea46301c133a8febc3771
    Signed-off-by: huaqin87 <huaqin87@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2138669
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Wei Xu <xuwei9@lenovo.com>
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key

--- drivers/power/mmi_qc3p_wt6670f/Android.mk
+++ drivers/power/mmi_qc3p_wt6670f/Android.mk
@@ -1,6 +1,10 @@
 DLKM_DIR := motorola/kernel/modules
 LOCAL_PATH := $(call my-dir)
 
+ifeq ($(MMI_QC3P_TURBO_CHARGER),true)
+	KERNEL_CFLAGS += CONFIG_MMI_QC3P_TURBO_CHARGER_ISP_5G=y
+endif
+
 include $(CLEAR_VARS)
 LOCAL_MODULE := mmi_qc3p_wt6670f.ko
 LOCAL_MODULE_TAGS := optional
