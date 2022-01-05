198ae567d6f7f0e100411fb6c96920b5a2971f5e
zhangh12 <zhangh12@motorola.com>
2022-01-05 14:16:56 +0800

    sar: support sar reset after usb hot-plug
    1. This driver is porting from MTK, however, on QC platform, the sys
       node is "usb" only.
    2. modify the correct macro
    
    Change-Id: Ib2a799ce28571a274c1432f2aef842bd9f1ecf32
    Signed-off-by: zhangh12 <zhangh12@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2162018
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key

--- drivers/misc/aw9610x/Android.mk
+++ drivers/misc/aw9610x/Android.mk
@@ -1,8 +1,8 @@
 DLKM_DIR := motorola/kernel/modules
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(SX933X_USB_USE_ONLINE),true)
-	KBUILD_OPTIONS += CONFIG_SX933X_POWER_SUPPLY_ONLINE=y
+ifeq ($(AW9610X_USB_USE_ONLINE),true)
+	KBUILD_OPTIONS += CONFIG_AW9610X_POWER_SUPPLY_ONLINE=y
 endif
 
 include $(CLEAR_VARS)
