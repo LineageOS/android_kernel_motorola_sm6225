ea7cfdd4d8aaa2e9f4ad9e51bc6e5492d09c3044
mahj8 <mahj8@motorola.com>
2022-01-29 16:46:59 +0800

    charGing: report power max 30W when usb type is qc3p
    As "turbopower connected" appears twice, so report power max
    30W when usb type is qc3p.
    
    Change-Id: Ife81a9763022d74079b091eed0b33c535885e57b
    Signed-off-by: mahj8 <mahj8@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2181964
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Submit-Approved: Jira Key

--- drivers/power/mmi_discrete_charger/mmi_discrete_charger_core.c
+++ drivers/power/mmi_discrete_charger/mmi_discrete_charger_core.c
@@ -2262,9 +2262,10 @@ static int mmi_discrete_get_chg_info(void *data, struct mmi_charger_info *chg_in
 			chip->chg_info.chrg_pmax_mw = chip->constraint.dcp_pmax;
 		else if (usb_type == POWER_SUPPLY_TYPE_USB_HVDCP)
 			chip->chg_info.chrg_pmax_mw = 7500;
-		else if (usb_type == POWER_SUPPLY_TYPE_USB_HVDCP_3 ||
-			usb_type == POWER_SUPPLY_TYPE_USB_HVDCP_3P5)
+		else if (usb_type == POWER_SUPPLY_TYPE_USB_HVDCP_3)
 			chip->chg_info.chrg_pmax_mw = chip->constraint.hvdcp_pmax;
+		else if (usb_type == POWER_SUPPLY_TYPE_USB_HVDCP_3P5)
+			chip->chg_info.chrg_pmax_mw = 30000;
 		else if (usb_type == POWER_SUPPLY_TYPE_USB_PD)
 			chip->chg_info.chrg_pmax_mw = chip->constraint.pd_pmax;
 		else
