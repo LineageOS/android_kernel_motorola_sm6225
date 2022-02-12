2df206bdf27cb0f4a8498f66b7fc0161939f740f
kankan17 <kankan17@motorola.com>
2022-02-12 11:55:41 +0800

    hawao: Actuator noise reduction
    details:
    To eliminate the resonance noise during system vibrator vibrating, set actuator to
    DAC 0 position,then the resonance noise can be obviously improved. To achieve
    the combination between vibrator and camera AF actuator,we implement:
    - Actuator separately control during camera power off.
    
    Change-Id: Ic2cca45b23c39fb08a20749cf8cac09dad9d4778
    Signed-off-by: kankan17 <kankan17@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2189515
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Shuo Wang <wangsh18@motorola.com>
    Reviewed-by: <liuzy55@motorola.com>
    Reviewed-by: Long Cheng <chengl1@motorola.com>
    Reviewed-by: PengFei Qiao <qiaopf1@motorola.com>
    Reviewed-by: <chenlj26@motorola.com>
    Reviewed-by: Zhuoran Xu <xuzr3@motorola.com>
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    Submit-Approved: Jira Key

--- drivers/cam_sensor_module/cam_actuator/mot_actuator.c
+++ drivers/cam_sensor_module/cam_actuator/mot_actuator.c
@@ -30,7 +30,7 @@
 
 /*=================ACTUATOR HW INFO====================*/
 #define DEVICE_NAME_LEN 32
-#define MAX_ACTUATOR_NUM 3
+#define MAX_ACTUATOR_NUM 4
 #define REGULATOR_NAME_MAX_LEN 32
 #define LENS_MAX_STAGES 10
 
@@ -53,6 +53,7 @@ typedef enum {
 	MOT_DEVICE_NIO,
 	MOT_DEVICE_CAPRIP,
 	MOT_DEVICE_RHODE,
+	MOT_DEVICE_HAWAO,
 	MOT_DEVICE_NUM,
 } mot_dev_type;
 
@@ -229,6 +230,31 @@ static const mot_dev_info mot_dev_list[MOT_DEVICE_NUM] = {
 				.cci_dev = 0x00,
 				.cci_master = 0x0,
 				.regulator_list = {"ldo4", "ldo3"},
+				.regulator_volt_uv = {1800000, 2800000},
+				.park_lens_needed = true,
+				.launch_lens = {
+						.launch_lens_needed = true,
+						.launch_lens_step = {
+									{300, 150},
+									{200, 100},
+									{100, 62}
+					},
+				},
+			},
+		},
+	},
+	{
+		.dev_type = MOT_DEVICE_HAWAO,
+		.dev_name = "hawao",
+		.actuator_info = {
+			[0] = {
+				.actuator_type = MOT_ACTUATOR_DW9800V,
+				.dac_pos = 0,
+				.init_pos = 512,
+				.cci_addr = 0x0c,
+				.cci_dev = 0x00,
+				.cci_master = 0x0,
+				.regulator_list = {"ldo7", "ldo5"},
 				.regulator_volt_uv = {1800000, 2800000},
 				.park_lens_needed = true,
 				.launch_lens = {
