ab8c2ed4271d5ba5a17c5cbdb9204c469c977e77
cuijy1 <cuijy2@lenovo.com>
2022-02-13 18:01:30 +0800

    sar:add driver for multi sx937x
    add driver for multi sx937x
    
    Change-Id: I0ec688cec7b7773f0ab4d578189e51da26122a4d
    Signed-off-by: cuijy1 <cuijy2@mt.com>
    Reviewed-on: https://gerrit.mot.com/2188738
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Tao Sun <suntao2@motorola.com>
    Submit-Approved: Jira Key

---
+++ drivers/misc/sx937x_multi/Android.mk
@@ -0,0 +1,11 @@
+DLKM_DIR := motorola/kernel/modules
+LOCAL_PATH := $(call my-dir)
+
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := sx937x_multi.ko
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
+LOCAL_ADDITIONAL_DEPENDENCIES := $(KERNEL_MODULES_OUT)/sensors_class.ko
+KBUILD_OPTIONS_GKI += GKI_OBJ_MODULE_DIR=gki
+include $(DLKM_DIR)/AndroidKernelModule.mk
