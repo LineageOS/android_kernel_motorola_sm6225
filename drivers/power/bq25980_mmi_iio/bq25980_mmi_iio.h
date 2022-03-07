e209d5a2145eb4fcb7b9e6763e7b8fbc8c244d73
wanggc6 <wanggc6@motorola.com>
2022-03-07 21:13:13 +0800

    charging:	add iio sys support
    Add  iio sys support for bq25960
    and some STAT MASK .
    Uncomment the function bq25980_set_chg_en,
    bq25980_get_adc_ibus and bq25980_get_adc_vbus.
    Because they are used by the iio sys.
    
    Change-Id: I528d6eb020c55a78b3bd575d8ce48eab656a9553
    Signed-off-by: wanggc6 <wanggc6@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2209068
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Submit-Approved: Jira Key

---
+++ drivers/power/bq25980_mmi_iio/bq25980_mmi_iio.h
@@ -0,0 +1,50 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/* Copyright (C) 2020 Texas Instruments Incorporated - http://www.ti.com/ */
+
+#ifndef BQ25980_IIO_H
+#define BQ25980_IIO_H
+
+
+#include <linux/iio/iio.h>
+#include <dt-bindings/iio/qti_power_supply_iio.h>
+
+
+struct bq25980_iio_channels {
+	const char *datasheet_name;
+	int channel_num;
+	enum iio_chan_type type;
+	long info_mask;
+};
+
+#define bq25980_IIO_CHAN(_name, _num, _type, _mask)		\
+	{						\
+		.datasheet_name = _name,		\
+		.channel_num = _num,			\
+		.type = _type,				\
+		.info_mask = _mask,			\
+	},
+
+#define bq25980_CHAN_INDEX(_name, _num)			\
+	bq25980_IIO_CHAN(_name, _num, IIO_INDEX,		\
+		BIT(IIO_CHAN_INFO_PROCESSED))
+
+#define bq25980_CHAN_CUR(_name, _num)			\
+	bq25980_IIO_CHAN(_name, _num, IIO_CURRENT,		\
+		BIT(IIO_CHAN_INFO_PROCESSED))
+
+#define bq25980_CHAN_VOLT(_name, _num)			\
+	bq25980_IIO_CHAN(_name, _num, IIO_VOLTAGE,		\
+		BIT(IIO_CHAN_INFO_PROCESSED))
+
+static const struct bq25980_iio_channels bq25980_iio_psy_channels[] = {
+	bq25980_CHAN_INDEX("bq25980_cp_enabled", PSY_IIO_CP_ENABLE)
+	bq25980_CHAN_INDEX("bq25980_online", PSY_IIO_ONLINE)
+	bq25980_CHAN_CUR("bq25980_current_now", PSY_IIO_CURRENT_NOW)
+	bq25980_CHAN_VOLT("bq25980_voltage_now", PSY_IIO_VOLTAGE_NOW)
+	bq25980_CHAN_INDEX("bq25980_input_current_now", PSY_IIO_MMI_CP_INPUT_CURRENT_NOW)
+	bq25980_CHAN_INDEX("bq25980_input_voltage_settled", PSY_IIO_MMI_CP_INPUT_VOLTAGE_NOW)
+	bq25980_CHAN_INDEX("bq25980_cp_status1", PSY_IIO_CP_STATUS1)
+	bq25980_CHAN_INDEX("bq25980_cp_clear_error", PSY_IIO_CP_CLEAR_ERROR)
+};
+
+#endif /* BQ25980_IIO_H */
