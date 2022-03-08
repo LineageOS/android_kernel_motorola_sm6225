262f523a2e5c6527f929dedab716d806cb53d4b5
wingte49 <wingte49@motorola.com>
2022-03-08 10:43:35 +0800

    dlkm: chipone_tddi_fhd: add initial chipone fhd tddi driver
    chipone_tddi_fhd_mmi:
    Add initial fhd tddi touch driver. This driver is for icnl9922 and
    later fhd driver.
    
    Change-Id: I2cce6e8d624e56fbaae963c4da47ad0c063f7e65
    Reviewed-on: https://gerrit.mot.com/2209608
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Yeqing Wang <wangyq13@motorola.com>
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Reviewed-by: Jun Weng <wengjun1@motorola.com>
    Submit-Approved: panyq6 <panyq6@motorola.com>

---
+++ drivers/input/touchscreen/chipone_tddi_fhd_mmi/cts_test.h
@@ -0,0 +1,17 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef CTS_TEST_H
+#define CTS_TEST_H
+
+struct cts_device;
+
+extern int cts_short_test(struct cts_device *cts_dev, u16 threshold);
+extern int cts_open_test(struct cts_device *cts_dev, u16 threshold);
+extern int cts_reset_test(struct cts_device *cts_dev);
+extern int cts_compensate_cap_test(struct cts_device *cts_dev, u8 min_thres,
+				   u8 max_thres);
+extern int cts_rawdata_test(struct cts_device *cts_dev, u16 min_thres,
+			    u16 max_thres);
+extern int cts_noise_test(struct cts_device *cts_dev, u32 frames, u16 max);
+extern int cts_test_int_pin(struct cts_device *cts_dev);
+
+#endif /* CTS_TEST_H */
