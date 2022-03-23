04eee253636d036be7fd7b2860d9f31ec6a551d3
wangyq13 <wangyq13@lenovo.com>
2022-03-23 14:23:52 +0800

    dlkm: leds/aw2033: add initial version driver
    leds/aw2033:
    Add initial version driver.
    
    Change-Id: Ieed6e2725c6c514259ad6d036b8a961ce4994840
    Signed-off-by: wangyq13 <wangyq13@mt.com>
    Reviewed-on: https://gerrit.mot.com/2223864
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Haijian Ma <mahj8@motorola.com>
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Submit-Approved: Jira Key

--- drivers/leds/aw2033/Android.mk
+++ drivers/leds/aw2033/Android.mk
@@ -2,9 +2,7 @@ DLKM_DIR := motorola/kernel/modules
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := leds-indicator-pwm.ko
+LOCAL_MODULE := leds_aw2033.ko
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
 include $(DLKM_DIR)/AndroidKernelModule.mk
-
-include $(call first-makefiles-under,$(LOCAL_PATH))
