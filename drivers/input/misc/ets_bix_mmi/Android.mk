a508396ba3decfcf506f2519d8a4fd2aa02b88c3
liujian24 <liujian24@lenovo.com>
2022-03-29 14:27:17 +0800

    Devon:Bringup Egistec FPS ETS_617
    WHY:Bringup Egistec FPS ETS_617
    WHAT:Add ETS_617 drivers
    
    Change-Id: Ibe4851fe447e1abea93222a698d5b2cdcbbff276
    Reviewed-on: https://gerrit.mot.com/2228365
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: liujian <liujian24@lenovo.com>
    Reviewed-by: Wang Wang <wangwang1@lenovo.com>
    Submit-Approved: Jira Key

---
+++ drivers/input/misc/ets_bix_mmi/Android.mk
@@ -0,0 +1,19 @@
+DLKM_DIR := motorola/kernel/modules
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(DLKM_INSTALL_TO_VENDOR_OUT),true)
+ETS_FPS_MMI_MODULE_PATH := $(TARGET_OUT_VENDOR)/lib/modules/
+else
+ETS_FPS_MMI_MODULE_PATH := $(KERNEL_MODULES_OUT)
+endif
+
+ifeq ($(EGIS_FPS_PANEL_NOTIFICATIONS),true)
+    KERNEL_CFLAGS += CONFIG_EGIS_PANEL_NOTIFICATIONS=y
+endif
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := ets_bix_mmi.ko
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(ETS_FPS_MMI_MODULE_PATH)
+include $(DLKM_DIR)/AndroidKernelModule.mk
+
