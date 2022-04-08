fd236dadf87673910985a6ca0faa7602dd415330
longc102 <longc102@motorola.com>
2022-04-08 10:24:28 +0800

    devon:add rt9471 charger
    add rt9471 charger
    add rt9471 driver code
    
    Change-Id: I5455c4399e6ece136b622d7709670dc53a3df244
    Signed-off-by: longc102 <longc102@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2236857
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Qiong Xie <xieq2@motorola.com>
    Reviewed-by: Wang Wang <wangwang1@mt.com>
    Submit-Approved: Jira Key

---
+++ drivers/power/rt9471_chg_mmi/Android.mk
@@ -0,0 +1,21 @@
+DLKM_DIR := motorola/kernel/modules
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE := rt9471_charger.ko
+
+ifeq ($(MMI_QC3P_TURBO_CHARGER),true)
+        KERNEL_CFLAGS += CONFIG_MMI_QC3P_TURBO_CHARGER=y
+endif
+
+ifeq ($(MMI_QC3P_WT6670_DETECTED),true)
+        KERNEL_CFLAGS += CONFIG_MMI_QC3P_WT6670_DETECTED=y
+endif
+
+LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
+
+LOCAL_ADDITIONAL_DEPENDENCIES := $(KERNEL_MODULES_OUT)/mmi_discrete_charger_class.ko
+KBUILD_OPTIONS_GKI += GKI_OBJ_MODULE_DIR=gki
+
+include $(DLKM_DIR)/AndroidKernelModule.mk
