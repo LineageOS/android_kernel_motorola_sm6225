7c7177a3a4ab3168f4bd7238a6020d436aecaaf8
Haijian Ma <mahj8@motorola.com>
2022-05-24 21:22:23 -0500

    Revert "charging: Don't switch dpdm from AP to pmic when host mode"
    Revert first,
    because build error
    
    This reverts commit 07a95e30fedd835de2e1b0f2a61a0cd9e23fa10b.
    
    Change-Id: I42232ac2bff46e2808d7b0c26f30c021da8df4ee
    Reviewed-on: https://gerrit.mot.com/2279036
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Submit-Approved: Jira Key

--- drivers/usb/typec/mmi_tcpc/rt_pd_manager.c
+++ drivers/usb/typec/mmi_tcpc/rt_pd_manager.c
@@ -24,7 +24,6 @@
 #include <linux/iio/consumer.h>
 #include "inc/tcpci_typec.h"
 #include <linux/mmi_discrete_power_supply.h>
-#include <linux/mmi_discrete_charger_class.h>
 
 #define RT_PD_MANAGER_VERSION	"0.0.8_G"
 
@@ -69,8 +68,6 @@ struct rt_pd_manager_data {
 	struct typec_partner *partner;
 	struct typec_partner_desc partner_desc;
 	struct usb_pd_identity partner_identity;
-
-	struct charger_device	*master_chg_dev;
 };
 
 static const unsigned int rpm_extcon_cable[] = {
@@ -166,31 +163,9 @@ out:
 	return ret;
 }
 
-static void mmi_ignore_require_dpdm(struct rt_pd_manager_data *rpmd, bool value)
-{
-	int rc = 0;
-
-	if (!rpmd->master_chg_dev)
-		rpmd->master_chg_dev = get_charger_by_name("master_chg");
-
-	if (!rpmd->master_chg_dev)
-		return;
-
-	if (value) {
-		rc = charger_dev_set_dp_dm(rpmd->master_chg_dev,
-				MMI_POWER_SUPPLY_IGNORE_REQUEST_DPDM);
-		dev_info(rpmd->dev, "%s ignore dp dm request rc=%d\n", rc? "Couldn't" : " ", rc);
-	} else {
-		rc = charger_dev_set_dp_dm(rpmd->master_chg_dev,
-				MMI_POWER_SUPPLY_DONOT_IGNORE_REQUEST_DPDM);
-		dev_info(rpmd->dev, "%s enable dp dm request rc=%d\n", rc? "Couldn't" : " ", rc);
-	}
-}
-
 static inline void stop_usb_host(struct rt_pd_manager_data *rpmd)
 {
 	extcon_set_state_sync(rpmd->extcon, EXTCON_USB_HOST, false);
-	mmi_ignore_require_dpdm(rpmd, false);
 }
 
 static inline void start_usb_host(struct rt_pd_manager_data *rpmd)
@@ -206,7 +181,6 @@ static inline void start_usb_host(struct rt_pd_manager_data *rpmd)
 			    EXTCON_PROP_USB_SS, val);
 
 	extcon_set_state_sync(rpmd->extcon, EXTCON_USB_HOST, true);
-	mmi_ignore_require_dpdm(rpmd, true);
 }
 
 static inline void stop_usb_peripheral(struct rt_pd_manager_data *rpmd)
