ee4421fff028c8895814a4307cc892bb1b5d84e3
chenlj2 <chenlj2@motorola.com>
2022-08-24 11:29:37 +0800

    dlkm:optimise bu250xx bulild rule
    solve build error
    cp: cannot stat 'out/target/product/genevn/obj/DLKM_OBJ/
    motorola/kernel/modules/drivers/misc/pen/bu520xx_pen.ko':
    No such file or directory
    
    Change-Id: I0c19135b807a2492cb0144fe4ba26b3053b70c66
    Signed-off-by: chenlj2 <chenlj2@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2369677
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Jiacheng Ou <oujc1@motorola.com>
    Reviewed-by: Minghui Wei <weimh1@motorola.com>
    Reviewed-by: Tao Sun <suntao2@motorola.com>
    Submit-Approved: Jira Key

--- drivers/misc/pen/Android.mk
+++ drivers/misc/pen/Android.mk
@@ -5,4 +5,5 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := bu520xx_pen.ko
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
+KBUILD_OPTIONS_GKI += GKI_OBJ_MODULE_DIR=gki
 include $(DLKM_DIR)/AndroidKernelModule.mk
