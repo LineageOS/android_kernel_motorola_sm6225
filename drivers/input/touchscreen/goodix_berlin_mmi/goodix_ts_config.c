381ced2f2660eeb29b3b339a786fc619e97ff86c
Wenge Zhu <zhuwg1@motorola.com>
2023-09-11 14:42:09 +0800

    Enable touch high report rate for ctwo
    Enable touch high report rate for ctwo.
    normal mode:240HZ
    game mode with sensitive touch:360HZ
    
    Change-Id: Ibb2cef93ea6152dd8372974775dfccf3232e864f
    Signed-off-by: Wenge Zhu <zhuwg1@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2735798
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Jichao Zou <zoujc@motorola.com>
    Submit-Approved: Jira Key

--- drivers/input/touchscreen/goodix_berlin_mmi/goodix_ts_config.c
+++ drivers/input/touchscreen/goodix_berlin_mmi/goodix_ts_config.c
@@ -15,7 +15,8 @@
 struct goodix_ic_report_rate_config report_rate_config_info = {
 #if defined(PRODUCT_LI) || defined(PRODUCT_ONELI) || defined(PRODUCT_TUNDRA) \
 	||defined(PRODUCT_EQS) || defined(PRODUCT_RTWO) || defined(PRODUCT_BRONCO) \
-	|| defined(PRODUCT_ZEEKR) || defined(PRODUCT_LYNKCO) || defined(PRODUCT_BANGKK)
+	|| defined(PRODUCT_ZEEKR) || defined(PRODUCT_LYNKCO) || defined(PRODUCT_BANGKK) \
+	|| defined(PRODUCT_CTWO)
 	.rate_config_count = 2,
 	.refresh_rate_ctrl = 0,
 	.interpolation_ctrl = 1,
